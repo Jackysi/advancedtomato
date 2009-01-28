a804fcc02a5425a86d773aab67eb0cab32ef1582
Fedor <fedork@ubuntu.(none)>
2009-01-27 20:40:01 -0500

    uClibc patches

--- tools-src/uClibc/libc/sysdeps/linux/common/ftruncate64.c
+++ tools-src/uClibc/libc/sysdeps/linux/common/ftruncate64.c
@@ -17,38 +17,65 @@
 #include <errno.h>
 #include <endian.h>
 #include <stdint.h>
+#include <sys/types.h>
 #include <sys/syscall.h>
+
+#if defined __UCLIBC_HAS_LFS__
 
 #if defined __NR_ftruncate64
 
-#if __WORDSIZE == 64 || (defined(__powerpc__) && defined (__UCLIBC_HAS_LFS__))
+#if __WORDSIZE == 64
 
 /* For a 64 bit machine, life is simple... */
 _syscall2(int, ftruncate64, int, fd, __off64_t, length);
 
 #elif __WORDSIZE == 32
 
-#if defined __UCLIBC_HAS_LFS__
-
 #ifndef INLINE_SYSCALL
 #define INLINE_SYSCALL(name, nr, args...) __syscall_ftruncate64 (args)
 #define __NR___syscall_ftruncate64 __NR_ftruncate64
-static inline _syscall3(int, __syscall_ftruncate64, int, fd, int, high_length, int, low_length);
+#if defined(__powerpc__) || defined(__mips__)
+static inline _syscall4(int, __syscall_ftruncate64, int, fd, uint32_t, pad,
+	unsigned long, high_length, unsigned long, low_length);
+#else
+static inline _syscall3(int, __syscall_ftruncate64, int, fd,
+	unsigned long, high_length, unsigned long, low_length);
 #endif
-
+#endif
 
 /* The exported ftruncate64 function.  */
 int ftruncate64 (int fd, __off64_t length)
 {
     uint32_t low = length & 0xffffffff;
     uint32_t high = length >> 32;
-    return INLINE_SYSCALL(ftruncate64, 3, fd, __LONG_LONG_PAIR (high, low));
+#if defined(__powerpc__) || defined(__mips__)
+    return INLINE_SYSCALL(ftruncate64,
+	    4, fd, 0, __LONG_LONG_PAIR (high, low));
+#else
+    return INLINE_SYSCALL(ftruncate64, 3, fd,
+	    __LONG_LONG_PAIR (high, low));
+#endif
 }
-#endif /* __UCLIBC_HAS_LFS__ */
 
 #else /* __WORDSIZE */
 #error Your machine is not 64 bit or 32 bit, I am dazed and confused.
 #endif /* __WORDSIZE */
 
-#endif
+#else  /* __NR_ftruncate64 */
 
+int ftruncate64 (int fd, __off64_t length)
+{
+	__off_t x = (__off_t) length;
+
+	if (x == length) {
+		return ftruncate(fd, x);
+	}
+
+	__set_errno((x < 0) ? EINVAL : EFBIG);
+
+	return -1;
+}
+
+#endif /* __NR_ftruncate64 */
+
+#endif /* __UCLIBC_HAS_LFS__ */
