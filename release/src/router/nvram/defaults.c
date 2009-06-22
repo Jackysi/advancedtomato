7ba5193bff07f25444258a306c82b8f28c3e1275
Victek <Victek@gmail.com>
2009-06-21 22:06:29 -0500

    Changes to NVRAM default values

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -125,7 +125,7 @@ const defaults_t defaults[] = {
 	{ "wl_corerev",			""				},	// Current core revision
 	{ "wl_phytypes",		""				},	// List of supported wireless bands (e.g. "ga")
 	{ "wl_radioids",		""				},	// List of radio IDs
-	{ "wl_ssid",			"wireless"		},	// Service set ID (network name)
+	{ "wl_ssid",			"TomatoRAF"		},	// Service set ID (network name)
 	{ "wl_country",			"Worldwide"		},	// Country (default obtained from driver)
 	{ "wl_radio",			"1"				},	// Enable (1) or disable (0) radio
 	{ "wl_closed",			"0"				},	// Closed (hidden) network
@@ -142,7 +142,7 @@ const defaults_t defaults[] = {
 	{ "wl_key3",			""				},	// 5/13 char ASCII or 10/26 char hex
 	{ "wl_key4",			""				},	// 5/13 char ASCII or 10/26 char hex
 	{ "wl_maclist",			""				},	// xx:xx:xx:xx:xx:xx ...
-	{ "wl_channel",			"6"				},	// Channel number
+	{ "wl_channel",			"10"				},	// Channel number
 	{ "wl_rate",			"0"				},	// Rate (bps, 0 for auto)
 	{ "wl_mrate",			"0"				},	// Mcast Rate (bps, 0 for auto)
 	{ "wl_rateset",			"default"		},	// "default" or "all" or "12"
@@ -238,12 +238,12 @@ const defaults_t defaults[] = {
 	{ "wan_domain",			""				},
 
 // basic-time
-	{ "tm_sel",				"PST8PDT,M3.2.0/2,M11.1.0/2"	},
-	{ "tm_tz",				"PST8PDT,M3.2.0/2,M11.1.0/2"	},
+	{ "tm_sel",				"CET-1CEST,M3.5.0/2,M10.5.0/3"	},
+	{ "tm_tz",				"CET-1CEST,M3.5.0/2,M10.5.0/3"	},
 	{ "tm_dst",				"1",							},
 	{ "ntp_updates",		"4"								},
 	{ "ntp_tdod",			"0"								},
-	{ "ntp_server",			"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org" },
+	{ "ntp_server",			"0.europe.pool.ntp.org 1.europe.pool.ntp.org 2.europe.pool.ntp.org" },
 	{ "ntp_kiss",			""								},
 	{ "ntp_kiss_ignore",	""								},
 
@@ -256,9 +256,9 @@ const defaults_t defaults[] = {
 	{ "macnames",			""				},
 
 // advanced-ctnf
-	{ "ct_tcp_timeout",		""				},
-	{ "ct_udp_timeout",		""				},
-	{ "ct_max",				""				},
+	{ "ct_tcp_timeout",		"180 1200 120 60 120 120 10 60 30 120" },
+	{ "ct_udp_timeout",		"30 180"		},
+	{ "ct_max",				"8192"			},
 	{ "nf_ttl",				"0"				},
 	{ "nf_l7in",			"1"				},
 	{ "nf_rtsp",			"1"				},
@@ -315,7 +315,7 @@ const defaults_t defaults[] = {
 	{ "dmz_sip",			""				},
 
 // forward-upnp
-	{ "upnp_enable",		"1"				},
+	{ "upnp_enable",		"0"				},
 	{ "upnp_ssdp_interval",	"60"			},	// SSDP interval
 	{ "upnp_max_age",		"180"			},	// Max age
 	{ "upnp_mnp",			"0"				},
@@ -342,8 +342,8 @@ const defaults_t defaults[] = {
 	{ "qos_orates",			"80-100,10-100,5-100,3-100,2-95,1-50,1-40,1-30,1-20,1-10"	},
 	
 	{ "ne_vegas",			"0"				},	// TCP Vegas
-	{ "ne_valpha",			"2"				},	// "
-	{ "ne_vbeta",			"6"				},	// "
+	{ "ne_valpha",			"3"				},	// "
+	{ "ne_vbeta",			"3"				},	// "
 	{ "ne_vgamma",			"2"				},	// "
 
 // access restrictions
@@ -377,7 +377,7 @@ const defaults_t defaults[] = {
 	{ "https_crt",			""				},
 	{ "web_wl_filter",		"0"				},	// Allow/Deny Wireless Access Web
 	{ "web_favicon",		"0"				},
-	{ "web_css",			"tomato"		},
+	{ "web_css",			"enlightened"		},
 	{ "web_svg",			"1"				},
 	{ "telnetd_eas",		"1"				},
 	{ "telnetd_port",		"23"			},
@@ -406,14 +406,12 @@ const defaults_t defaults[] = {
 // advanced-buttons
 	{ "sesx_led",			"0"				},
 	{ "sesx_b0",			"1"				},
-	{ "sesx_b1",			"4"				},
-	{ "sesx_b2",			"4"				},
+	{ "sesx_b1",			"2"				},
+	{ "sesx_b2",			"3"				},
 	{ "sesx_b3",			"4"				},
-	{ "sesx_b4",			"4"				},
-	{ "sesx_script",
-		"[ $1 -ge 20 ] && telnetd -p 233 -l /bin/sh\n"
-	},
-	{ "sesx_script_1",		""				},
+	{ "sesx_b4",			"5"				},
+	{ "sesx_script",		"[ $1 -ge 20 ] && telnetd -p 233 -l /bin/sh\n" },
+	{ "sesx_script_1",		"service wan restart"		},
 	{ "sesx_script_2",		""				},
 	{ "script_brau",
 		"if [ ! -e /tmp/switch-start ]; then\n"
@@ -447,7 +445,7 @@ const defaults_t defaults[] = {
 	{ "debug_cprintf_file",	"0"				},
 //	{ "debug_keepfiles",	"0"				},
 	{ "console_loglevel",	"1"				},
-	{ "t_cafree",			"0"				},
+	{ "t_cafree",			"1"				},
 	{ "t_hidelr",			"0"				},
 	{ "debug_clkfix",		"1"				},
 	{ "debug_ddns",			"0"				},
@@ -463,10 +461,10 @@ const defaults_t defaults[] = {
 // admin-sch
 	{ "sch_rboot",			""				},
 	{ "sch_rcon",			""				},
-	{ "sch_c1",				""				},
+	{ "sch_c1",				"1,-1440,127"	},
 	{ "sch_c2",				""				},
 	{ "sch_c3",				""				},
-	{ "sch_c1_cmd",			""				},
+	{ "sch_c1_cmd",			"service upnp restart" },
 	{ "sch_c2_cmd",			""				},
 	{ "sch_c3_cmd",			""				},
 
