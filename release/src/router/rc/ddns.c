f6b79c23dca51c6a0471510ff3462d82de423c89
Fedor <fedork@ubuntu.(none)>
2009-09-03 21:52:57 -0400

    ddns update fix in case cron is not started yet

--- release/src/router/rc/ddns.c
+++ release/src/router/rc/ddns.c
@@ -196,9 +196,8 @@ static void update(int num, int *dirty, int force)
 		t += (n * 86400);	// refresh every n days
 		
 		//!!TB - fix: if time is in the past, make it current
-		time_t now;
-		time(&now);
-		if (t < now + 60) t = now + 60;
+		time_t now = time(0) + (60 * 5);
+		if (t < now) t = now;
 
 		tm = localtime(&t);
 		sprintf(s, "cru a ddnsf%d \"%d %d %d %d * ddns-update %d force\"", num,
