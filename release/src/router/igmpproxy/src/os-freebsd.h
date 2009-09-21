5d3c636eb2126542073f2d430c34c544dd597c77
Fedor <fedork@ubuntu.(none)>
2009-09-20 23:37:26 -0400

    Replaced pre-built igmprt by igmpproxy

---
+++ release/src/router/igmpproxy/src/os-freebsd.h
@@ -0,0 +1,15 @@
+#include <net/route.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip_mroute.h>
+#include <netinet/ip.h>
+#include <netinet/igmp.h>
+
+static inline u_short ip_data_len(const struct ip *ip)
+{
+	return ip->ip_len;
+}
+
+static inline void ip_set_len(struct ip *ip, u_short len)
+{
+	ip->ip_len = len;
+}
