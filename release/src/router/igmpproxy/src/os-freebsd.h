7b1bb67ede38a0f6a4fea418bb826f592cb6a1c9
Fedor <fedork@ubuntu.(none)>
2009-10-06 17:41:10 -0400

    igmpproxy 0.1

--- release/src/router/igmpproxy/src/os-freebsd.h
+++ release/src/router/igmpproxy/src/os-freebsd.h
@@ -4,6 +4,14 @@
 #include <netinet/ip.h>
 #include <netinet/igmp.h>
 
+#if __FreeBSD_version >= 800069 && defined BURN_BRIDGES \
+	|| __FreeBSD_version >= 800098
+#define IGMP_MEMBERSHIP_QUERY IGMP_HOST_MEMBERSHIP_QUERY
+#define IGMP_V1_MEMBERSHIP_REPORT IGMP_v1_HOST_MEMBERSHIP_REPORT
+#define IGMP_V2_MEMBERSHIP_REPORT IGMP_v2_HOST_MEMBERSHIP_REPORT
+#define IGMP_V2_LEAVE_GROUP IGMP_HOST_LEAVE_MESSAGE
+#endif
+
 static inline u_short ip_data_len(const struct ip *ip)
 {
 	return ip->ip_len;
