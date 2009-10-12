2815bc786e3cf29046c9eeded7bf9a1690f44a15
Fedor <fedork@ubuntu.(none)>
2009-10-12 00:42:49 -0400

    Cleaner FUSE integration

--- release/src/linux/linux/fs/fuse/config.h
+++ release/src/linux/linux/fs/fuse/config.h
@@ -1,28 +1,28 @@
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* config.h for kernel 2.4.37.  */
 
 /* Kernel has i_size_read() and i_size_write() functions */
-#undef HAVE_I_SIZE_FUNC
+/* #undef HAVE_I_SIZE_FUNC */
 
 /* Kernel has xattr support */
-#undef HAVE_KERNEL_XATTR
+#define HAVE_KERNEL_XATTR 1
 
 /* lookup_instantiate_filp() is defined */
-#undef HAVE_LOOKUP_INSTANTIATE_FILP
+/* #undef HAVE_LOOKUP_INSTANTIATE_FILP */
 
 /* Kernel has recalc_sigpending_tsk() function */
-#undef HAVE_RECALC_SIGPENDING_TSK
+/* #undef HAVE_RECALC_SIGPENDING_TSK */
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "fuse-kernel"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "fuse-kernel 2.5.3"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "fuse-kernel"
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "2.5.3"
