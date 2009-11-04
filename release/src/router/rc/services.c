9d5bdb69ec144b40789d1a6fb7da927d8bf61ff7
Fedor <fedork@ubuntu.(none)>
2009-11-03 23:21:04 -0500

    Revert "Allow multiple MAC addresses per device" - to use new implementation from Tomato 1.26 beta
    This reverts commit 2027f068d7446cd344bd189ccf40635af5a0a705.

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -186,7 +186,7 @@ void start_dnsmasq()
 	p = nvram_safe_get("dhcpd_static");
 	while ((e = strchr(p, '>')) != NULL) {
 		n = (e - p);
-		if (n > sizeof(buf)-1) {
+		if (n > 84) {
 			p = e + 1;
 			continue;
 		}
