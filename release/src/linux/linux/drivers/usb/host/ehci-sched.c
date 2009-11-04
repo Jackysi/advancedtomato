010c41355c4d0cd18cd8b407fce3aa870c8708b9
Sarah Sharp <sarah.a.sharp@linux.intel.com>
2009-11-03 20:42:35 -0500

    USB EHCI: Minor constant fix for SCHEDULE_SLOP

--- release/src/linux/linux/drivers/usb/host/ehci-sched.c
+++ release/src/linux/linux/drivers/usb/host/ehci-sched.c
@@ -1316,7 +1316,7 @@ sitd_slot_ok (
  * given EHCI_TUNE_FLS and the slop).  Or, write a smarter scheduler!
  */
 
-#define SCHEDULE_SLOP	10	/* frames */
+#define SCHEDULE_SLOP	80	/* microframes */
 
 static int
 iso_stream_schedule (
@@ -1330,7 +1330,7 @@ iso_stream_schedule (
 	unsigned		mod = ehci->periodic_size << 3;
 	struct ehci_iso_sched	*sched = urb->hcpriv;
 
-	if (unlikely (sched->span > (mod - 8 * SCHEDULE_SLOP))) {
+	if (unlikely (sched->span > (mod - SCHEDULE_SLOP))) {
 		ehci_dbg (ehci, "iso request %p too long\n", urb);
 		status = -EFBIG;
 		goto fail;
@@ -1363,7 +1363,7 @@ iso_stream_schedule (
 			start += mod;
 
 		/* Fell behind (by up to twice the slop amount)? */
-		if (start >= max - 2 * 8 * SCHEDULE_SLOP)
+		if (start >= max - 2 * SCHEDULE_SLOP)
 			start += period * DIV_ROUND_UP(
 					max - start, period) - mod;
 
@@ -1382,7 +1382,7 @@ iso_stream_schedule (
 	 * can also help high bandwidth if the dma and irq loads don't
 	 * jump until after the queue is primed.
 	 */
-	start = SCHEDULE_SLOP * 8 + (now & ~0x07);
+	start = SCHEDULE_SLOP + (now & ~0x07);
 	start %= mod;
 	stream->next_uframe = start;
 
