204242f21b6c6a164ff00de9dacbc6715f30bf4f
Keith Moyer <tomatovpn@keithmoyer.com>
2009-12-19 15:27:00 -0500

    Upgrade to OpenVPN 2.1rc21

--- release/src/router/openvpn/config.h
+++ release/src/router/openvpn/config.h
@@ -446,13 +446,13 @@
 #define PACKAGE_NAME "OpenVPN"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "OpenVPN 2.1_rc20"
+#define PACKAGE_STRING "OpenVPN 2.1_rc21"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "openvpn"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.1_rc20"
+#define PACKAGE_VERSION "2.1_rc21"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -543,7 +543,7 @@
 /* #undef USE_VALGRIND */
 
 /* Version number of package */
-#define VERSION "2.1_rc20"
+#define VERSION "2.1_rc21"
 
 /* Enable GNU extensions on systems that have them.  */
 #ifndef _GNU_SOURCE
