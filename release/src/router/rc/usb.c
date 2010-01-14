caec98f4894c6f85627892d2d92f03a197db804c
Fedor <fedork@ubuntu.(none)>
2010-01-14 00:10:27 -0500

    Replace spaces in mountpoints by underscores

--- release/src/router/rc/usb.c
+++ release/src/router/rc/usb.c
@@ -465,6 +465,10 @@ int mount_partition(char *dev_name, int host_num, int disc_num, int part_num, ui
 	}
 
 	if (*the_label != 0) {
+		char *p;
+		for (p = the_label; *p; p++) {
+			if (*p == ' ') *p = '_';
+		}
 		sprintf(mountpoint, "%s/%s", MOUNT_ROOT, the_label);
 		if ((ret = mount_r(dev_name, mountpoint, type))) {
 			if (ret == MOUNT_VAL_RONLY || ret == MOUNT_VAL_RW)
