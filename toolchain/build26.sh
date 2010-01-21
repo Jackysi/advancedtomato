367999479c527eb7237cde0816dbf946cecc185c
Fedor <fedork@ubuntu.(none)>
2010-01-21 02:47:36 -0500

    Toolchain: kernel 2.4 - switch to gcc 4.1.2 for userspace apps

--- toolchain/build26.sh
+++ toolchain/build26.sh
@@ -20,7 +20,12 @@ ln -nsf mipsel-linux-uclibc-gcc-${GCCVER} mipsel-uclibc-gcc-${GCCVER}
 cd /opt/brcm
 rm -f hndtools-mipsel-linux
 rm -f hndtools-mipsel-uclibc
-ln -nsf hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-linux
-ln -nsf hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-uclibc
+
+mkdir -p K26
+rm -rf K26/hndtools-mipsel-uclibc-${GCCVER}
+mv -f hndtools-mipsel-uclibc-${GCCVER} K26/
+
+ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-linux
+ln -nsf K26/hndtools-mipsel-uclibc-${GCCVER} hndtools-mipsel-uclibc
 
 cd $ROOTDIR
