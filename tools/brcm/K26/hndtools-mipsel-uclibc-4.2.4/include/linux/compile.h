0d11ffaae3d322d0b9f331bef4443a7700f9fe06
Fedor <fedork@ubuntu.(none)>
2010-01-28 02:28:58 -0500

    Toolchain: binary toolchain for kernel 2.6 with gcc 4.2.4

---
+++ tools/brcm/K26/hndtools-mipsel-uclibc-4.2.4/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#1 Wed Jan 27 23:18:55 EST 2010"
+#define LINUX_COMPILE_TIME "23:18:55"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "tomato"
+#define LINUX_COMPILE_DOMAIN ""
+#define LINUX_COMPILER "gcc version 4.1.2"
