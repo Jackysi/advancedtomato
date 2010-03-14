721bf03b638b24c8703c055a5d9a61582d3147ae
Fedor <fedork@ubuntu.(none)>
2010-03-14 17:56:49 -0400

    ntfs-3g: additional compile fixes for ver. 2010-3-6

--- release/src/router/ntfs-3g/src/ntfs-3g.c
+++ release/src/router/ntfs-3g/src/ntfs-3g.c
@@ -870,8 +870,7 @@ static int ntfs_fuse_getattr(const char *org_path, struct stat *stbuf)
  	stbuf->st_atim = ntfs2timespec(ni->last_access_time);
  	stbuf->st_ctim = ntfs2timespec(ni->last_mft_change_time);
  	stbuf->st_mtim = ntfs2timespec(ni->last_data_change_time);
-#else
-	/* No known timespec member in struct stat */
+#elif defined(HAVE_STRUCT_STAT_ST_ATIMENSEC)
 	struct timespec ts;
 	ts = ntfs2timespec(ni->last_access_time);
 	stbuf->st_atime = ts.tv_sec;
@@ -882,6 +881,8 @@ static int ntfs_fuse_getattr(const char *org_path, struct stat *stbuf)
 	ts = ntfs2timespec(ni->last_data_change_time);
 	stbuf->st_mtime = ts.tv_sec;
 	stbuf->st_mtimensec = ts.tv_nsec;
+#else
+	#error "No known timespec member in struct stat!"
 #endif
 exit:
 	if (ntfs_inode_close(ni))
