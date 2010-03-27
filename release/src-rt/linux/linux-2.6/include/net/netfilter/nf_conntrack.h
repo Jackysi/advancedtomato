2200338be526305b380d2f72a77f1731318b6b96
Fedor <fedork@ubuntu.(none)>
2010-03-27 01:33:41 -0400

    Netfilter: fix position error adding new nf_conn members

--- release/src-rt/linux/linux-2.6/include/net/netfilter/nf_conntrack.h
+++ release/src-rt/linux/linux-2.6/include/net/netfilter/nf_conntrack.h
@@ -28,6 +28,23 @@
 
 #include <net/netfilter/nf_conntrack_tuple.h>
 
+#if defined(CONFIG_NETFILTER_XT_MATCH_LAYER7) || \
+    defined(CONFIG_NETFILTER_XT_MATCH_LAYER7_MODULE)
+struct ip_ct_layer7
+{
+	/*
+	 * e.g. "http". NULL before decision. "unknown" after decision
+	 * if no match.
+	 */
+	char *app_proto;
+	/*
+	 * application layer data so far. NULL after match decision.
+	 */
+	char *app_data;
+	unsigned int app_data_len;
+};
+#endif
+
 /* per conntrack: protocol private data */
 union nf_conntrack_proto {
 	/* insert conntrack proto private data here */
@@ -135,24 +152,13 @@ struct nf_conn
 	/* Storage reserved for other modules: */
 	union nf_conntrack_proto proto;
 
-	/* features dynamically at the end: helper, nat (both optional) */
-	char data[0];
-
 #if defined(CONFIG_NETFILTER_XT_MATCH_LAYER7) || \
     defined(CONFIG_NETFILTER_XT_MATCH_LAYER7_MODULE)
-	struct {
-		/*
-		 * e.g. "http". NULL before decision. "unknown" after decision
-		 * if no match.
-		 */
-		char *app_proto;
-		/*
-		 * application layer data so far. NULL after match decision.
-		 */
-		char *app_data;
-		unsigned int app_data_len;
-	} layer7;
+	struct ip_ct_layer7 layer7;
 #endif
+
+	/* features dynamically at the end: helper, nat (both optional) */
+	char data[0];
 };
 
 static inline struct nf_conn *
