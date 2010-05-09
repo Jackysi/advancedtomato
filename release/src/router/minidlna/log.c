b423a99b86368c5eb06859ba9aa16de8fd917ae0
Fedor <fedork@ubuntu.(none)>
2010-05-09 01:19:21 -0400

    MiniDLNA: use syslog instead of a log file

--- release/src/router/minidlna/log.c
+++ release/src/router/minidlna/log.c
@@ -14,15 +14,22 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#define SYSLOG
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
+#ifdef SYSLOG
+#include <syslog.h>
+#endif
 
 #include "log.h"
 
+#ifndef SYSLOG
 static FILE *log_fp = NULL;
+#endif
 static int default_log_level = E_WARN;
 int log_level[L_MAX];
 
@@ -49,11 +56,25 @@ char *level_name[] = {
 	0
 };
 
+#ifdef SYSLOG
+int level_prio[] = {
+	LOG_NOTICE,				// E_OFF
+	LOG_CRIT,				// E_FATAL
+	LOG_ERR,				// E_ERROR
+	LOG_WARNING,				// E_WARN
+	LOG_INFO,				// E_INFO
+	LOG_DEBUG,				// E_DEBUG
+	0
+};
+#endif
+
 int
 log_init(const char *fname, const char *debug)
 {
 	int i;
+#ifndef SYSLOG
 	FILE *fp;
+#endif
 	short int log_level_set[L_MAX];
 
 	if (debug)
@@ -104,12 +125,26 @@ log_init(const char *fname, const char *debug)
 			log_level[i] = default_log_level;
 	}
 
+#ifdef SYSLOG
+	int openlog_option;
+
+	openlog_option = LOG_PID | LOG_CONS;
+	if (!fname)
+		openlog_option |= LOG_PERROR;	/* also log on stderr */
+	openlog("minidlna", openlog_option, LOG_DAEMON);
+
+	if (fname) {
+		/* speed things up and ignore LOG_INFO and LOG_DEBUG */
+		setlogmask(LOG_UPTO(LOG_NOTICE));
+	}
+#else
 	if (!fname)					// use default i.e. stdout
 		return 0;
 
 	if (!(fp = fopen(fname, "a")))
 		return 1;
 	log_fp = fp;
+#endif
 	return 0;
 }
 
@@ -119,14 +154,18 @@ log_err(int level, enum _log_facility facility, char *fname, int lineno, char *f
 	//char errbuf[1024];
 	char * errbuf;
 	va_list ap;
+#ifndef SYSLOG
 	time_t t;
 	struct tm *tm;
+#endif
 
 	if (level && level>log_level[facility] && level>E_FATAL)
 		return;
 
+#ifndef SYSLOG
 	if (!log_fp)
 		log_fp = stdout;
+#endif
 
 	// user log
 	va_start(ap, fmt);
@@ -134,6 +173,12 @@ log_err(int level, enum _log_facility facility, char *fname, int lineno, char *f
 	vasprintf(&errbuf, fmt, ap);
 	va_end(ap);
 
+#ifdef SYSLOG
+	if (log_level[facility] < E_DEBUG)
+		syslog(level_prio[level], errbuf);
+	else
+		syslog(level_prio[level], "%s:%d: %s", fname, lineno, errbuf);
+#else
 	// timestamp
 	t = time(NULL);
 	tm = localtime(&t);
@@ -146,6 +191,7 @@ log_err(int level, enum _log_facility facility, char *fname, int lineno, char *f
 	else
 		fprintf(log_fp, "%s:%d: %s", fname, lineno, errbuf);
 	fflush(log_fp);
+#endif
 	free(errbuf);
 
 	if (level==E_FATAL)
