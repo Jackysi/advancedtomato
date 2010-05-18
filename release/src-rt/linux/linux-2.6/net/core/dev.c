c37cab626c33ea9681f18c5c6ff6aa3ae38bf0eb
Herbert Xu <herbert@gondor.apana.org.au>
2010-05-18 00:40:31 -0400

    NET: Call uninit if necessary in register_netdevice
    kernel.org commit 7ce1b0edcb11f90f6fc5e0ceecff467f329889a0

--- release/src-rt/linux/linux-2.6/net/core/dev.c
+++ release/src-rt/linux/linux-2.6/net/core/dev.c
@@ -3155,7 +3155,7 @@ int register_netdevice(struct net_device *dev)
 
 	if (!dev_valid_name(dev->name)) {
 		ret = -EINVAL;
-		goto out;
+		goto err_uninit;
 	}
 
 	dev->ifindex = dev_new_index();
@@ -3169,7 +3169,7 @@ int register_netdevice(struct net_device *dev)
 			= hlist_entry(p, struct net_device, name_hlist);
 		if (!strncmp(d->name, dev->name, IFNAMSIZ)) {
 			ret = -EEXIST;
-			goto out;
+			goto err_uninit;
 		}
 	}
 
@@ -3213,7 +3213,7 @@ int register_netdevice(struct net_device *dev)
 
 	ret = netdev_register_sysfs(dev);
 	if (ret)
-		goto out;
+		goto err_uninit;
 	dev->reg_state = NETREG_REGISTERED;
 
 	/*
@@ -3238,6 +3238,11 @@ int register_netdevice(struct net_device *dev)
 
 out:
 	return ret;
+
+err_uninit:
+	if (dev->uninit)
+		dev->uninit(dev);
+	goto out;
 }
 
 /**
