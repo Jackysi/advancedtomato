4a9088f1ac5f004acb12d4a71dc5280654f143a0
Oleg Nesterov <oleg@redhat.com>
2010-06-07 15:06:47 -0400

    tty: release_one_tty() forgets to put pids kernel.org commit 6da8d866d0d39e9509ff826660f6a86a6757c966. Patch from wl500g.googlecode.com

--- release/src-rt/linux/linux-2.6/drivers/char/tty_io.c
+++ release/src-rt/linux/linux-2.6/drivers/char/tty_io.c
@@ -2262,6 +2262,8 @@ static void release_one_tty(struct tty_struct *tty, int idx)
 	list_del_init(&tty->tty_files);
 	file_list_unlock();
 
+	put_pid(tty->pgrp);
+	put_pid(tty->session);
 	free_tty_struct(tty);
 }
 
