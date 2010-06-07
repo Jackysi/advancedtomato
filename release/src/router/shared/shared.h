Jackysi/advancedtomato

commit 03ca6e6fb5db0546a92e70bcfba526653b3d5e40

Linksys WRT320N ID

release/src/router/shared/shared.h
@@ -153,6 +153,7 @@ enum {
 	MODEL_WNR3500L,
 	MODEL_WNR2000v2,
 	MODEL_WRT160Nv3,
+	MODEL_WRT320N,
 	MODEL_MN700,
 	MODEL_WRH54G,
 	MODEL_WHRG125,
@@ -182,6 +183,7 @@ enum {
 	HW_BCM5350,
 	HW_BCM5356,
 	HW_BCM4716,
+	HW_BCM4717,
 	HW_BCM4718,
 	HW_UNKNOWN
 };

release/src/router/rc/buttons.c
@@ -165,6 +165,10 @@ int buttons_main(int argc, char *argv[])
 		reset_mask = 1 << 6;
 		ses_mask = 1 << 5;
 		break;
+	case MODEL_WRT320N:
+		reset_mask = 1 << 8;
+		ses_mask = 1 << 5;
+		break;
 #endif
 	default:
 		get_btn("btn_ses", &ses_mask, &ses_pushed);

release/src/router/rc/init.c
@@ -430,6 +430,15 @@ static void check_bootnv(void)
 			dirty |= check_nv("vlan1ports", "4 3 2 1 5*");
 		}
 		break;
+	case MODEL_WRT320N:
+		if (nvram_match("clkdivsf", "2")) {
+			// fix lan port numbering
+			dirty |= check_nv("vlan1ports", "4 3 2 1 8*");
+			dirty |= check_nv("vlan2ports", "0 8");
+		}
+		dirty |= check_nv("ledbh0", "136");
+		dirty |= check_nv("ledbh1", "11");
+		break;
 #endif
 
 	case MODEL_WRT54G:
@@ -846,6 +855,17 @@ static int init_nvram(void)
 		else
 			name = "WRT160N v3";
 		features = SUP_SES | SUP_80211N | SUP_WHAM_LED;
+		if (!nvram_match("t_fix1", (char *)name)) {
+			nvram_set("lan_ifnames", "vlan1 eth1");
+			nvram_set("wan_ifnameX", "vlan2");
+			nvram_set("wl_ifname", "eth1");
+			nvram_set("t_fix1", name);
+		}
+		break;
+	case MODEL_WRT320N:
+		mfr = "Linksys";
+		name = "WRT320N";
+		features = SUP_SES | SUP_80211N | SUP_WHAM_LED | SUP_1000ET;
 		if (!nvram_match("t_fix1", (char *)name)) {
 			nvram_set("lan_ifnames", "vlan1 eth1");
 			nvram_set("wan_ifnameX", "vlan2");

release/src/router/shared/id.c
@@ -29,6 +29,7 @@ WRT300N 1.0         BCM4704_BCM5325F_EWC  0x0472       42        0x10      0x10
 WRTSL54GS           BCM4704_BCM5325F      0x042f       42        0x10      0x0018
 WTR54GS v1, v2      BCM5350               0x456        56        0x10      0xb18       (source: BaoWeiQuan)
 WRT160Nv3, M10      BCM4716               0x04cd       42        0x1700                boot_hw_model=WRT160N boot_hw_ver=3.0 (M10: boot_hw_model=M10 boot_hw_ver=1.0)
+WRT320N             BCM4717               0x04ef       42/66     0x1304/0x1305         boardflags: 0x0040F10 / 0x00000602 (??)
 
 WHR-G54S            BCM5352E              0x467        00        0x13      0x2758      melco_id=30182
 WHR-HP-G54S         BCM5352E              0x467        00        0x13      0x2758      melco_id=30189
@@ -130,6 +131,8 @@ int check_hw_type(void)
 	case 0x04cd:
 	case 0xe4cd:
 		return HW_BCM4716;
+	case 0x04ef:
+		return HW_BCM4717;
 	case 0x04cf:
 		return HW_BCM4718;
 #endif
@@ -225,6 +228,8 @@ int get_model(void)
 #ifdef CONFIG_BCMWL5
 		case HW_BCM4716:
 			return MODEL_WRT160Nv3;
+		case HW_BCM4717:
+			return MODEL_WRT320N;
 #endif
 		}
 		return MODEL_WRT54G;
@@ -250,6 +255,12 @@ int get_model(void)
 		}
 		break;
 #ifdef CONFIG_BCMWL5
+	case 66:
+		switch (hw) {
+		case HW_BCM4717:
+			return MODEL_WRT320N;
+		}
+		break;
 	case 1:
 		switch (hw) {
 		case HW_BCM4716:

release/src/router/shared/led.c
@@ -193,6 +193,7 @@ int led(int which, int mode)
 	static int wnr3500[]	= { 255, 255,     2,  255,  255,   -1,  255,  255	};
 	static int wnr2000v2[]	= { 255,   2,     6,  255,  255,   -7,  255,  255	};
 	static int wrt160nv3[]	= { 255,   1,     4,    2,  255,  255,  255,  255	};
+	static int wrt320n[]	= { 255,   2,     3,    4,  255,  255,  255,  255	};
 #endif
 
 	char s[16];
@@ -304,6 +305,9 @@ int led(int which, int mode)
 		break;
 	case MODEL_WRT160Nv3:
 		b = wrt160nv3[which];
+		break;
+	case MODEL_WRT320N:
+		b = wrt320n[which];
 		break;
 #endif
 /*

commit 5c53c44b75fbb5033afb12733c4fee04176fdf4e

Fix hardware IDs ordering.
Commit 03ca6e6fb5db0546a92e70bcfba526653b3d5e40 broke the save/restore
configuration for BCM4718 routers by adding a new hardware ID in the
middle of the enum. Fix it, and add a comment to prevent this in th future.

release/src/router/shared/shared.h
@@ -170,6 +170,11 @@ enum {
 #endif
 };
 
+/* NOTE: Do not insert new entries in the middle of this enum,
+ * always add them at the end! The numeric Hardware ID value is
+ * stored in the configuration file, and is used to determine
+ * whether or not this config file can be restored on the router.
+ */
 enum {
 	HW_BCM4702,
 	HW_BCM4712,
@@ -183,8 +188,8 @@ enum {
 	HW_BCM5350,
 	HW_BCM5356,
 	HW_BCM4716,
-	HW_BCM4717,
 	HW_BCM4718,
+	HW_BCM4717,
 	HW_UNKNOWN
 };
 