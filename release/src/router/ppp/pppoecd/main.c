8b1e33ed4ae2aa063cd408e2ad1499b4744575f7
Polaris75 <polaris75>
2010-06-28 03:28:04 -0400

    pppoecd: add optional IPv6 support

--- release/src/router/ppp/pppoecd/main.c
+++ release/src/router/ppp/pppoecd/main.c
@@ -47,6 +47,9 @@
 #include "fsm.h"
 #include "lcp.h"
 #include "ipcp.h"
+#ifdef INET6
+#include "ipv6cp.h"
+#endif
 #include "upap.h"
 #include "chap.h"
 #include "ccp.h"
@@ -199,6 +202,9 @@ struct protent *protocols[] = {
     &chap_protent,
 #endif
     &ipcp_protent,
+#ifdef INET6
+    &ipv6cp_protent,
+#endif
 #ifdef CCP_SUPPORT
     &ccp_protent,
 #endif
