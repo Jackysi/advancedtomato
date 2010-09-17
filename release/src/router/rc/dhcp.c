Jackysi/advancedtomato

commit a5542e84325843a2c81f852ff4b452adbe341e59

rc: simplify processing of dhcp routes

release/src/router/rc/dhcp.c
@@ -106,7 +106,7 @@ static int deconfig(char *ifname)
 static int renew(char *ifname)
 {
 	char *a, *b;
-	int changed, routes_changed;
+	int changed, routes_changed = 0;
 
 	TRACE_PT("begin\n");
 
@@ -139,20 +139,10 @@ static int renew(char *ifname)
 	nvram_set("wan_routes_save", nvram_safe_get("wan_routes"));
 	nvram_set("wan_msroutes_save", nvram_safe_get("wan_msroutes"));
 
-	/* RFC3442: If the DHCP server returns both a Classless Static Routes option
-	 * and a Router option, the DHCP client MUST ignore the Router option.
-	 * Similarly, if the DHCP server returns both a Classless Static Routes
-	 * option and a Static Routes option, the DHCP client MUST ignore the
-	 * Static Routes option.
-	 * Read more: http://www.faqs.org/rfcs/rfc3442.html
-	 */
-	routes_changed = env2nv("staticroutes", "wan_routes_save");
-	if (routes_changed)
-		nvram_set("wan_msroutes_save", "");
-	else {
-		routes_changed |= env2nv("routes", "wan_routes_save");
-		routes_changed |= env2nv("msroutes", "wan_msroutes_save");
-	}
+	/* Static Routes */
+	routes_changed |= env2nv("routes", "wan_routes_save");
+	/* MS Classless Static Routes */
+	routes_changed |= env2nv("msstaticroutes", "wan_msroutes_save");
 	changed |= routes_changed;
 
 	if ((a = getenv("lease")) != NULL) {
@@ -206,11 +196,12 @@ static int bound(char *ifname)
 	 * Similarly, if the DHCP server returns both a Classless Static Routes
 	 * option and a Static Routes option, the DHCP client MUST ignore the
 	 * Static Routes option.
+	 * Read more: http://www.faqs.org/rfcs/rfc3442.html
 	 */
-	if (!env2nv("staticroutes", "wan_routes")) {
-		env2nv("routes", "wan_routes");
-		env2nv("msroutes", "wan_msroutes");
-	}
+	/* Static Routes */
+	env2nv("routes", "wan_routes");
+	/* MS Classless Static Routes */
+	env2nv("msstaticroutes", "wan_msroutes");
 
 	expires(atoi(safe_getenv("lease")));
 

release/src/router/rc/wan.c
@@ -551,20 +551,15 @@ void force_to_dial(void)
 
 // -----------------------------------------------------------------------------
 
-void do_wan_routes(char *ifname, int metric, int add)
+static void _do_wan_routes(char *ifname, char *nvname, int metric, int add)
 {
 	char *routes, *tmp;
-	int bit, bits;
-	struct in_addr ip, gw, mask;
-
-	char ipaddr[16];
-	char gateway[16];
+	int bits;
+	struct in_addr mask;
 	char netmask[16];
 
-	if (!nvram_get_int("dhcp_routes")) return;
-
-	// staticroutes or routes
-	tmp = routes = strdup(nvram_safe_get("wan_routes"));
+	// IP[/MASK] ROUTER IP2[/MASK2] ROUTER2 ...
+	tmp = routes = strdup(nvram_safe_get(nvname));
 	while (tmp && *tmp) {
 		char *ipaddr, *gateway, *nmask;
 
@@ -589,42 +584,15 @@ void do_wan_routes(char *ifname, int metric, int add)
 		}
 	}
 	free(routes);
+}
 
-	// ms routes
-	routes = nvram_get("wan_msroutes");
-	while (routes && isdigit(*routes)) {
-		// read net length
-		bits = strtol(routes, &routes, 10);
-		if (bits < 1 || bits > 32 || *routes != ' ')
-			break;
-		mask.s_addr = htonl(0xffffffff << (32 - bits));
-
-		// read network address
-		for (ip.s_addr = 0, bit = 24; bit > (24 - bits); bit -= 8) {
-			if (*routes++ != ' ' || !isdigit(*routes))
-				return;
-			ip.s_addr |= htonl(strtol(routes, &routes, 10) << bit);
-		}
-
-		// read gateway
-		for (gw.s_addr = 0, bit = 24; bit >= 0 && *routes; bit -= 8) {
-			if (*routes++ != ' ' || !isdigit(*routes))
-				return;
-			gw.s_addr |= htonl(strtol(routes, &routes, 10) << bit);
-		}
-
-		// clear bits per RFC
-		ip.s_addr &= mask.s_addr;
-
-		strcpy(ipaddr, inet_ntoa(ip));
-		strcpy(gateway, inet_ntoa(gw));
-		strcpy(netmask, inet_ntoa(mask));
-
-		if (add) route_add(ifname, metric + 1, ipaddr, gateway, netmask);
-		else route_del(ifname, metric + 1, ipaddr, gateway, netmask);
-
-		if (*routes == ' ')
-			routes++;
+void do_wan_routes(char *ifname, int metric, int add)
+{
+	if (nvram_get_int("dhcp_routes")) {
+		// Static Routes: IP ROUTER IP2 ROUTER2 ...
+		_do_wan_routes(ifname, "wan_routes",   metric, add);
+		// MS Classless Static Routes: IP/MASK ROUTER IP2/MASK2 ROUTER2 ...
+		_do_wan_routes(ifname, "wan_msroutes", metric, add);
 	}
 }
 

commit cc1bb4ef9397250c5cd721ef637d40f50b52b53f

rc: fix support for DHCP classless static routes broken by commit a5542e84325843a2c81f852ff4b452adbe341e59

release/src/router/rc/dhcp.c
@@ -106,7 +106,7 @@ static int deconfig(char *ifname)
 static int renew(char *ifname)
 {
 	char *a, *b;
-	int changed, routes_changed = 0;
+	int changed, routes_changed;
 
 	TRACE_PT("begin\n");
 
@@ -139,8 +139,10 @@ static int renew(char *ifname)
 	nvram_set("wan_routes_save", nvram_safe_get("wan_routes"));
 	nvram_set("wan_msroutes_save", nvram_safe_get("wan_msroutes"));
 
+	/* Classless Static Routes */
+	routes_changed = env2nv("staticroutes", "wan_routes_save");
 	/* Static Routes */
-	routes_changed |= env2nv("routes", "wan_routes_save");
+	if (!routes_changed) routes_changed = env2nv("routes", "wan_routes_save");
 	/* MS Classless Static Routes */
 	routes_changed |= env2nv("msstaticroutes", "wan_msroutes_save");
 	changed |= routes_changed;
@@ -198,10 +200,9 @@ static int bound(char *ifname)
 	 * Static Routes option.
 	 * Read more: http://www.faqs.org/rfcs/rfc3442.html
 	 */
-	/* Static Routes */
-	env2nv("routes", "wan_routes");
-	/* MS Classless Static Routes */
-	env2nv("msstaticroutes", "wan_msroutes");
+	if (!env2nv("staticroutes", "wan_routes"))	/* Classless Static Routes */
+		env2nv("routes", "wan_routes");		/* Static Routes */
+	env2nv("msstaticroutes", "wan_msroutes");	/* MS Classless Static Routes */
 
 	expires(atoi(safe_getenv("lease")));
 