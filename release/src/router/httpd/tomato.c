f8e6d160799c12e4e63e6d0eff113e4cfb3c1726
Toastman <kallaya9@truemail.co.th>
2010-09-19 01:12:44 +0700

    Fixed CPU frequency

--- release/src/router/httpd/tomato.c
+++ release/src/router/httpd/tomato.c
@@ -641,7 +641,7 @@ static const nvset_t nvset_list[] = {
 // advanced-misc
 	{ "wait_time",			V_RANGE(3, 20)		},
 	{ "wan_speed",			V_RANGE(0, 4)		},
-	{ "clkfreq",                    V_RANGE(125,500)        },
+	{ "clkfreq",                    V_NONE		        },	// Removed check - KP
 	{ "jumbo_frame_enable",		V_01			},	// Jumbo Frames support (for RT-N16/WNR3500L)
 	{ "jumbo_frame_size",		V_RANGE(1, 9720)	},
 
