ba7a5bd6af8474dd200f27949e8c9cddebe94e8e
lly <lly@sf.net>
2010-11-02 19:11:49 -0400

    [TCP]: Don't panic if S+L skb is detected Backport from kernel.org commit b9c4595bc4947faa236a849324fe3492e388d949

--- release/src-rt/linux/linux-2.6/include/net/tcp.h
+++ release/src-rt/linux/linux-2.6/include/net/tcp.h
@@ -757,7 +757,7 @@ static inline __u32 tcp_current_ssthresh(const struct sock *sk)
 
 static inline void tcp_sync_left_out(struct tcp_sock *tp)
 {
-	BUG_ON(tp->rx_opt.sack_ok &&
+	WARN_ON(tp->rx_opt.sack_ok &&
 	       (tp->sacked_out + tp->lost_out > tp->packets_out));
 	tp->left_out = tp->sacked_out + tp->lost_out;
 }
