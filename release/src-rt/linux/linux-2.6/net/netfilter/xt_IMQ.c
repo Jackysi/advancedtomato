37dec6cfe290f6c748460768e8a528c149ded4f7
lly <lly@sf.net>
2010-11-12 16:52:31 -0500

    netfilter: merge ipt_IMQ & ip6t_IMQ into xt_IMQ

--- release/src-rt/linux/linux-2.6/net/netfilter/xt_IMQ.c
+++ release/src-rt/linux/linux-2.6/net/netfilter/xt_IMQ.c
@@ -3,8 +3,8 @@
  */
 #include <linux/module.h>
 #include <linux/skbuff.h>
-#include <linux/netfilter_ipv4/ip_tables.h>
-#include <linux/netfilter_ipv4/ipt_IMQ.h>
+#include <linux/netfilter/x_tables.h>
+#include <linux/netfilter/xt_IMQ.h>
 #include <linux/imq.h>
 
 static unsigned int imq_target(struct sk_buff **pskb,
@@ -14,7 +14,7 @@ static unsigned int imq_target(struct sk_buff **pskb,
 			       const struct xt_target *target,
 			       const void *targinfo)
 {
-	struct ipt_imq_info *mr = (struct ipt_imq_info*)targinfo;
+	struct xt_imq_info *mr = (struct xt_imq_info*)targinfo;
 
 	(*pskb)->imq_flags = mr->todev | IMQ_F_ENQUEUE;
 
@@ -27,9 +27,7 @@ static int imq_checkentry(const char *tablename,
 			  void *targinfo,
 			  unsigned int hook_mask)
 {
-	struct ipt_imq_info *mr;
-
-	mr = (struct ipt_imq_info*)targinfo;
+	struct xt_imq_info *mr = (struct xt_imq_info*)targinfo;
 
 	if (mr->todev > IMQ_MAX_DEVS) {
 		printk(KERN_WARNING
@@ -41,29 +39,42 @@ static int imq_checkentry(const char *tablename,
 	return 1;
 }
 
-static struct xt_target ipt_imq_reg = {
-	.name		= "IMQ",
-	.family		= AF_INET,
-	.target		= imq_target,
-	.targetsize	= sizeof(struct ipt_imq_info),
-	.checkentry	= imq_checkentry,
-	.me		= THIS_MODULE,
-	.table		= "mangle"
+static struct xt_target xt_imq_reg[] __read_mostly = {
+	{
+		.name		= "IMQ",
+		.family		= AF_INET,
+		.target		= imq_target,
+		.targetsize	= sizeof(struct xt_imq_info),
+		.checkentry	= imq_checkentry,
+		.me		= THIS_MODULE,
+		.table		= "mangle"
+	},
+	{
+		.name           = "IMQ",
+		.family		= AF_INET6,
+		.target         = imq_target,
+		.targetsize	= sizeof(struct xt_imq_info),
+		.table		= "mangle",
+		.checkentry     = imq_checkentry,
+		.me             = THIS_MODULE
+	},
 };
 
 static int __init init(void)
 {
-	return xt_register_target(&ipt_imq_reg);
+	return xt_register_targets(xt_imq_reg, ARRAY_SIZE(xt_imq_reg));
 }
 
 static void __exit fini(void)
 {
-	xt_unregister_target(&ipt_imq_reg);
+	xt_unregister_targets(xt_imq_reg, ARRAY_SIZE(xt_imq_reg));
 }
 
 module_init(init);
 module_exit(fini);
 
+MODULE_ALIAS("ipt_IMQ");
+MODULE_ALIAS("ip6t_IMQ");
 MODULE_AUTHOR("http://www.linuximq.net");
 MODULE_DESCRIPTION("Pseudo-driver for the intermediate queue device. See http://www.linuximq.net/ for more information.");
 MODULE_LICENSE("GPL");
