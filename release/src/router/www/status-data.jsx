33ccb08042d733a3f0ecbfca61110fb23e4135fa
Fedor <fedork@ubuntu.(none)>
2010-11-30 14:01:04 -0500

    Web GUI: cosmetics for PPTP/L2TP using DHCP

--- release/src/router/www/status-data.jsx
+++ release/src/router/www/status-data.jsx
@@ -7,7 +7,7 @@
 	No part of this file may be used without permission.
 */
 
-//<% nvram("ppp_get_ip,pptp_server_ip,router_name,wan_domain,wan_gateway,wan_gateway_get,wan_get_domain,wan_hostname,wan_hwaddr,wan_ipaddr,wan_netmask,wan_proto,wan_run_mtu,et0macaddr,lan_proto,lan_ipaddr,dhcp_start,dhcp_num,dhcpd_startip,dhcpd_endip,lan_netmask,wl_security_mode,wl_crypto,wl_mode,wl_wds_enable,wl_hwaddr,wl_net_mode,wl_radio,wl_channel,lan_gateway,wl_ssid,t_model_name,t_features"); %>
+//<% nvram("ppp_get_ip,pptp_server_ip,router_name,wan_domain,wan_gateway,wan_gateway_get,wan_get_domain,wan_hostname,wan_hwaddr,wan_ipaddr,wan_netmask,wan_proto,wan_run_mtu,et0macaddr,lan_proto,lan_ipaddr,dhcp_start,dhcp_num,dhcpd_startip,dhcpd_endip,lan_netmask,wl_security_mode,wl_crypto,wl_mode,wl_wds_enable,wl_hwaddr,wl_net_mode,wl_radio,wl_channel,lan_gateway,wl_ssid,t_model_name,t_features,pptp_dhcp"); %>
 //<% uptime(); %>
 //<% sysinfo(); %>
 //<% wlstats(); %>
@@ -58,8 +58,12 @@ do {
 	case 'l2tp':
 		if (stats.wanup) {
 			stats.wanip = nvram.ppp_get_ip;
-			if (nvram.wan_ipaddr != '' && nvram.wan_ipaddr != '0.0.0.0' && nvram.wan_ipaddr != nvram.ppp_get_ip)
-				stats.wanip += ' <small>(MAN: ' + nvram.wan_ipaddr + ')</small>';
+			if (nvram.pptp_dhcp == '1') {
+				if (nvram.wan_ipaddr != '' && nvram.wan_ipaddr != '0.0.0.0' && nvram.wan_ipaddr != stats.wanip)
+					stats.wanip += '&nbsp;&nbsp;<small>(DHCP: ' + nvram.wan_ipaddr + ')</small>';
+				if (nvram.wan_gateway != '' && nvram.wan_gateway != '0.0.0.0' && nvram.wan_gateway != stats.wangateway)
+					stats.wangateway += '&nbsp;&nbsp;<small>(DHCP: ' + nvram.wan_gateway + ')</small>';
+			}
 			if (stats.wannetmask == '0.0.0.0')
 				stats.wannetmask = '255.255.255.255';
 		}
