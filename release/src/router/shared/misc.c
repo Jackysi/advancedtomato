Jackysi/advancedtomato

commit 8a074b70f8a00e38fce53423e87138d2013abe2e

rc, httpd, libshared: rework usage of wanface/manface for PPP connections

release/src/router/shared/misc.c
@@ -307,24 +307,60 @@ int wait_action_idle(int n)
 
 // -----------------------------------------------------------------------------
 
+const wanface_list_t *get_wanfaces(void)
+{
+	static wanface_list_t wanfaces;
+	char *ip, *iface;
+	int defgw, proto;
+
+	wanfaces.count = 0;
+
+	switch ((proto = get_wan_proto())) {
+		case WP_PPTP:
+		case WP_L2TP:
+			defgw = nvram_get_int("ppp_defgw");
+			while (wanfaces.count < 2) {
+				if ((defgw && wanfaces.count == 0) || (!defgw && wanfaces.count == 1)) {
+					ip = nvram_safe_get("ppp_get_ip");
+					iface = nvram_safe_get("wan_iface");
+					if (!(*iface)) iface = "ppp+";
+				}
+				else /* if ((!defgw && wanfaces.count == 0) || (defgw && wanfaces.count == 1)) */ {
+					ip = nvram_safe_get("wan_ipaddr");
+					if ((!(*ip) || strcmp(ip, "0.0.0.0") == 0) && (wanfaces.count > 0))
+						iface = "";
+					else
+						iface = nvram_safe_get("wan_ifname");
+				}
+				strlcpy(wanfaces.iface[wanfaces.count].ip, ip, sizeof(wanfaces.iface[0].ip));
+				strlcpy(wanfaces.iface[wanfaces.count].name, iface, IFNAMSIZ);
+				++wanfaces.count;
+			}
+			break;
+		default:
+			ip = (proto == WP_DISABLED) ? "0.0.0.0" : nvram_safe_get("wan_ipaddr");
+			iface = nvram_safe_get("wan_ifname");
+			strlcpy(wanfaces.iface[wanfaces.count].ip, ip, sizeof(wanfaces.iface[0].ip));
+			strlcpy(wanfaces.iface[wanfaces.count++].name, iface, IFNAMSIZ);
+			break;
+	}
+
+	return &wanfaces;
+}
+
+const char *get_wanface(void)
+{
+	return (*get_wanfaces()).iface[0].name;
+}
+
 const char *get_wanip(void)
 {
-	const char *p;
-
 	if (!check_wanup()) return "0.0.0.0";
-	switch (get_wan_proto()) {
-	case WP_DISABLED:
-		return "0.0.0.0";
-	case WP_PPTP:
-	case WP_L2TP:
-		p = "ppp_get_ip";
-		break;
-	default:
-		p = "wan_ipaddr";
-		break;
-	}
-	return nvram_safe_get(p);
+
+	return (*get_wanfaces()).iface[0].ip;
 }
+
+// -----------------------------------------------------------------------------
 
 long get_uptime(void)
 {

release/src/router/httpd/ctnf.c
@@ -537,26 +537,12 @@ void asp_qrate(int argc, char **argv)
 	int n;
 	char comma;
 	char *a[1];
-	char *qface;
 
 	a[0] = "1";
 	asp_ctcount(1, a);
 
-	qface = nvram_safe_get("wan_iface");
-	switch (get_wan_proto()) {
-	case WP_PPTP:
-	case WP_L2TP:
-		if (nvram_get_int("ppp_defgw") == 0) {
-			e = nvram_safe_get("wan_ipaddr");
-			if (*e && strcmp(e, get_wanip()) != 0 && strcmp(e, "0.0.0.0") != 0) {
-				qface = nvram_safe_get("wan_ifname");
-			}
-		}
-		break;
-	}
-
 	memset(rates, 0, sizeof(rates));
-	sprintf(s, "tc -s class ls dev %s", qface);
+	sprintf(s, "tc -s class ls dev %s", get_wanface());
 	if ((f = popen(s, "r")) != NULL) {
 		n = 1;
 		while (fgets(s, sizeof(s), f)) {

release/src/router/httpd/ddns.c
@@ -21,21 +21,10 @@ void asp_ddnsx(int argc, char **argv)
 	time_t tt;
 	struct stat st;
 
-	switch (get_wan_proto()) {
-	case WP_PPTP:
-	case WP_L2TP:
-		p = "ppp_get_ip";
-		if (nvram_get_int("ppp_defgw")) break;
-		// else fall through
-	default:
-		p = "wan_ipaddr";
-		break;
-	}
-
 	web_printf(
 		"\nddnsx_ip = '%s';\n"
 		"ddnsx_msg = [",
-		nvram_safe_get(p));
+		get_wanip());
 
 	for (i = 0; i < 2; ++i) {
 		web_puts(i ? "','" : "'");
@@ -72,16 +61,5 @@ void asp_ddnsx(int argc, char **argv)
 
 void asp_ddnsx_ip(int argc, char **argv)
 {
-	const char *p;
-
-	switch (get_wan_proto()) {
-	case WP_PPTP:
-	case WP_L2TP:
-		p = "ppp_get_ip";
-		break;
-	default:
-		p = "wan_ipaddr";
-		break;
-	}
-	web_puts(nvram_safe_get(p));
+	web_puts(get_wanip());
 }

release/src/router/rc/ddns.c
@@ -101,18 +101,7 @@ static void update(int num, int *dirty, int force)
 		}
 	}
 	else if (inet_addr(ip) == -1) {
-		switch (get_wan_proto()) {
-		case WP_PPTP:
-		case WP_L2TP:
-			if (!nvram_get_int("ppp_defgw")) {
-				strcpy(ip, nvram_safe_get("wan_ipaddr"));
-				break;
-			}
-			// else fall through
-		default:
-			strcpy(ip, get_wanip());
-			break;
-		}
+		strcpy(ip, get_wanip());
 	}
 
 	sprintf(cache_fn, "%s.cache", ddnsx_path);

release/src/router/rc/firewall.c
@@ -29,15 +29,13 @@
 #include <arpa/inet.h>
 #include <dirent.h>
 
-char wanface[IFNAMSIZ + 1];
-char manface[IFNAMSIZ + 1];
+wanface_list_t wanfaces;
 char lanface[IFNAMSIZ + 1];
 #ifdef TCONFIG_IPV6
 char wan6face[IFNAMSIZ + 1];
 #endif
-char wanaddr[sizeof("xxx.xxx.xxx.xxx") + 1];
-char manaddr[sizeof("xxx.xxx.xxx.xxx") + 1];
 char lan_cclass[sizeof("xxx.xxx.xxx.") + 1];
+
 static int web_lanport;
 
 #ifdef DEBUG_IPTFILE
@@ -228,21 +226,20 @@ char **layer7_in;
 // should read the cached result and set the appropriate marks	-- zzz
 void ipt_layer7_inbound(void)
 {
-	int en;
+	int en, i;
 	char **p;
 
 	if (!layer7_in) return;
 
 	en = nvram_match("nf_l7in", "1");
 	if (en) {
-		ipt_write(
-			":L7in - [0:0]\n"
-			"-A FORWARD -i %s -j L7in\n",
-				wanface);
-		if (*manface)
-			ipt_write(
-				"-A FORWARD -i %s -j L7in\n",
-					manface);
+		ipt_write(":L7in - [0:0]\n");
+		for (i = 0; i < wanfaces.count; ++i) {
+			if (*(wanfaces.iface[i].name)) {
+				ipt_write("-A FORWARD -i %s -j L7in\n",
+					wanfaces.iface[i].name);
+			}
+		}
 	}
 
 	p = layer7_in;
@@ -310,20 +307,19 @@ static void save_webmon(void)
 
 static void ipt_webmon(void)
 {
-	int wmtype, clear;
+	int wmtype, clear, i;
 
 	if (!nvram_get_int("log_wm")) return;
 	wmtype = nvram_get_int("log_wmtype");
 	clear = nvram_get_int("log_wmclear");
 
-	ipt_write(
-		":monitor - [0:0]\n"
-		"-A FORWARD -o %s -j monitor\n",
-		wanface);
-	if (*manface)
-		ipt_write(
-			"-A FORWARD -o %s -j monitor\n",
-			manface);
+	ipt_write(":monitor - [0:0]\n");
+	for (i = 0; i < wanfaces.count; ++i) {
+		if (*(wanfaces.iface[i].name)) {
+			ipt_write("-A FORWARD -o %s -j monitor\n",
+				wanfaces.iface[i].name);
+		}
+	}
 
 	ipt_write(
 		"-A monitor -m webmon "
@@ -345,7 +341,7 @@ static void ipt_webmon(void)
 static void mangle_table(void)
 {
 	int ttl;
-	char *p;
+	char *p, *wanface;
 
 	ip46t_write(
 		"*mangle\n"
@@ -380,6 +376,8 @@ static void mangle_table(void)
 #else
 			modprobe("ipt_TTL");
 #endif
+			// set TTL on primary WAN iface only
+			wanface = wanfaces.iface[0].name;
 			ip46t_write(
 				"-I PREROUTING -i %s -j TTL --ttl-%s %d\n"
 				"-I POSTROUTING -o %s -j TTL --ttl-%s %d\n",
@@ -405,6 +403,7 @@ static void nat_table(void)
 	char src[64];
 	char t[512];
 	char *p, *c;
+	int i;
 
 	ipt_write("*nat\n"
 		":PREROUTING ACCEPT [0:0]\n"
@@ -417,20 +416,19 @@ static void nat_table(void)
 		strlcpy(lanaddr, nvram_safe_get("lan_ipaddr"), sizeof(lanaddr));
 		strlcpy(lanmask, nvram_safe_get("lan_netmask"), sizeof(lanmask));
 
-		// chain_wan_prerouting
-		if (wanup)
-			ipt_write("-A PREROUTING -d %s -j %s\n", wanaddr, chain_wan_prerouting);
-		if (*manaddr)
-			ipt_write("-A PREROUTING -d %s -j %s\n", manaddr, chain_wan_prerouting);
+		for (i = 0; i < wanfaces.count; ++i) {
+			if (*(wanfaces.iface[i].name)) {
+				// chain_wan_prerouting
+				if (wanup)
+					ipt_write("-A PREROUTING -d %s -j %s\n",
+						wanfaces.iface[i].ip, chain_wan_prerouting);
 
-		// Drop incoming packets which destination IP address is to our LAN side directly
-		ipt_write("-A PREROUTING -i %s -d %s/%s -j DROP\n",
-			wanface,
-			lanaddr, lanmask);	// note: ipt will correct lanaddr
-		if (*manface)
-			ipt_write("-A PREROUTING -i %s -d %s/%s -j DROP\n",
-				manface,
-				lanaddr, lanmask);	// note: ipt will correct lanaddr
+				// Drop incoming packets which destination IP address is to our LAN side directly
+				ipt_write("-A PREROUTING -i %s -d %s/%s -j DROP\n",
+					wanfaces.iface[i].name,
+					lanaddr, lanmask);	// note: ipt will correct lanaddr
+			}
+		}
 
 		if (wanup) {
 			if (nvram_match("dns_intcpt", "1")) {
@@ -479,9 +477,11 @@ static void nat_table(void)
 				ipt_write("-A %s -j upnp\n", chain_wan_prerouting);
 			}
 			else {
-				ipt_write("-A PREROUTING -i %s -j upnp\n", wanface);
-				if (*manface)
-					ipt_write("-A PREROUTING -i %s -j upnp\n", manface);
+				for (i = 0; i < wanfaces.count; ++i) {
+					if (*(wanfaces.iface[i].name)) {
+						ipt_write("-A PREROUTING -i %s -j upnp\n", wanfaces.iface[i].name);
+					}
+				}
 			}
 		}
 
@@ -508,15 +508,14 @@ static void nat_table(void)
 			break;
 		}
 #endif
-		if ((!wanup) || (nvram_get_int("net_snat") != 1)) {
-			ipt_write("-A POSTROUTING %s -o %s -j MASQUERADE\n", p, wanface);
-			if (*manface)
-				ipt_write("-A POSTROUTING %s -o %s -j MASQUERADE\n", p, manface);
-		}
-		else {
-			ipt_write("-A POSTROUTING %s -o %s -j SNAT --to-source %s\n", p, wanface, wanaddr);
-			if (*manface && *manaddr)
-				ipt_write("-A POSTROUTING %s -o %s -j SNAT --to-source %s\n", p, manface, manaddr);
+
+		for (i = 0; i < wanfaces.count; ++i) {
+			if (*(wanfaces.iface[i].name)) {
+				if ((!wanup) || (nvram_get_int("net_snat") != 1))
+					ipt_write("-A POSTROUTING %s -o %s -j MASQUERADE\n", p, wanfaces.iface[i].name);
+				else
+					ipt_write("-A POSTROUTING %s -o %s -j SNAT --to-source %s\n", p, wanfaces.iface[i].name, wanfaces.iface[i].ip);
+			}
 		}
 
 		switch (nvram_get_int("nf_loopback")) {
@@ -550,9 +549,11 @@ static void filter_input(void)
 	char *p, *c;
 
 	if ((nvram_get_int("nf_loopback") != 0) && (wanup)) {	// 0 = all
-		ipt_write("-A INPUT -i %s -d %s -j DROP\n", lanface, wanaddr);
-		if (*manaddr)
-			ipt_write("-A INPUT -i %s -d %s -j DROP\n", lanface, manaddr);
+		for (n = 0; n < wanfaces.count; ++n) {
+			if (*(wanfaces.iface[n].name)) {
+				ipt_write("-A INPUT -i %s -d %s -j DROP\n", lanface, wanfaces.iface[n].ip);
+			}
+		}
 	}
 
 	ipt_write(
@@ -715,9 +716,10 @@ static void filter_forward(void)
 	char src[64];
 	char t[512];
 	char *p, *c;
+	int i;
 
 	ipt_write(
-		"-A FORWARD -i %s -o %s -j ACCEPT\n"				// accept all lan to lan
+		"-A FORWARD -i %s -o %s -j ACCEPT\n"			// accept all lan to lan
 		"-A FORWARD -m state --state INVALID -j DROP\n",	// drop if INVALID state
 		lanface, lanface);
 
@@ -734,27 +736,28 @@ static void filter_forward(void)
 	ipt_write(
 		":wanin - [0:0]\n"
 		":wanout - [0:0]\n"
-		"-A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT\n"	// already established or related (via helper)
-		"-A FORWARD -i %s -j wanin\n"					// generic from wan
-		"-A FORWARD -o %s -j wanout\n"					// generic to wan
-		"-A FORWARD -i %s -j %s\n",					// from lan
-		wanface, wanface, lanface, chain_out_accept);
+		"-A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT\n");	// already established or related (via helper)
 
-	if (*manface)
-		ipt_write(
-			"-A FORWARD -i %s -j wanin\n"				// generic from wan
-			"-A FORWARD -o %s -j wanout\n",				// generic to wan
-			manface, manface);
+	for (i = 0; i < wanfaces.count; ++i) {
+		if (*(wanfaces.iface[i].name)) {
+			ipt_write(
+				"-A FORWARD -i %s -j wanin\n"			// generic from wan
+				"-A FORWARD -o %s -j wanout\n",			// generic to wan
+				wanfaces.iface[i].name, wanfaces.iface[i].name);
+		}
+	}
+
+	ipt_write("-A FORWARD -i %s -j %s\n",					// from lan
+		lanface, chain_out_accept);
 
 	if (nvram_get_int("upnp_enable") & 3) {
-		ipt_write(
-			":upnp - [0:0]\n"
-			"-A FORWARD -i %s -j upnp\n",
-			wanface);
-		if (*manface)
-			ipt_write(
-				"-A FORWARD -i %s -j upnp\n",
-				manface);
+		ipt_write(":upnp - [0:0]\n");
+		for (i = 0; i < wanfaces.count; ++i) {
+			if (*(wanfaces.iface[i].name)) {
+				ipt_write("-A FORWARD -i %s -j upnp\n",
+					wanfaces.iface[i].name);
+			}
+		}
 	}
 
 	if (wanup) {
@@ -990,7 +993,7 @@ int start_firewall(void)
 	DIR *dir;
 	struct dirent *dirent;
 	char s[256];
-	char *c;
+	char *c, *wanface;
 	int n;
 	int wanproto;
 	char *iptrestore_argv[] = { "iptables-restore", (char *)ipt_fname, NULL };
@@ -1048,23 +1051,8 @@ int start_firewall(void)
 
 	strlcpy(lanface, nvram_safe_get("lan_ifname"), IFNAMSIZ);
 
-	manaddr[0] = '\0';
-	manface[0] = '\0';
-
-	if ((wanproto == WP_PPTP) || (wanproto == WP_L2TP) || (wanproto == WP_PPPOE)) {
-		strcpy(wanface, "ppp+");
-	}
-	else {
-		strlcpy(wanface, nvram_safe_get("wan_ifname"), sizeof(wanface));
-	}
-
-	strlcpy(wanaddr, get_wanip(), sizeof(wanaddr));
-	c = nvram_safe_get("wan_ipaddr");
-	if (*c && strcmp(c, wanaddr) != 0 && strcmp(c, "0.0.0.0") != 0) {
-		strlcpy(manaddr, c, sizeof(manaddr));
-		if ((wanproto == WP_PPTP) || (wanproto == WP_L2TP) || (wanproto == WP_PPPOE))
-			strlcpy(manface, nvram_safe_get("wan_ifname"), sizeof(manface));
-	}
+	memcpy(&wanfaces, get_wanfaces(), sizeof(wanfaces));
+	wanface = wanfaces.iface[0].name;
 
 #ifdef TCONFIG_IPV6
 	switch (get_ipv6_service()) {

release/src/router/rc/forward.c
@@ -19,7 +19,7 @@ void ipt_forward(ipt_table_t table)
 	const char *proto, *saddr, *xports, *iport, *iaddr;
 	const char *c;
 	const char *mdport;
-	int i;
+	int i, n;
 	char ip[64];
 	char src[64];
 
@@ -78,21 +78,16 @@ void ipt_forward(ipt_table_t table)
 						ip,  *iport ? ":" : "", iport);
 
 					if (nvram_get_int("nf_loopback") == 1) {
-						ipt_write("-A POSTROUTING -p %s %s %s -s %s/%s -d %s -j SNAT --to-source %s\n",
-							c,
-							mdport, *iport ? iport : xports,
-							nvram_safe_get("lan_ipaddr"),	// corrected by ipt
-							nvram_safe_get("lan_netmask"),
-							ip,
-							wanaddr);
-						if (*manaddr) {
-							ipt_write("-A POSTROUTING -p %s %s %s -s %s/%s -d %s -j SNAT --to-source %s\n",
-								c,
-								mdport, *iport ? iport : xports,
-								nvram_safe_get("lan_ipaddr"),	// corrected by ipt
-								nvram_safe_get("lan_netmask"),
-								ip,
-								manaddr);
+						for (n = 0; n < wanfaces.count; ++n) {
+							if (*(wanfaces.iface[n].name)) {
+								ipt_write("-A POSTROUTING -p %s %s %s -s %s/%s -d %s -j SNAT --to-source %s\n",
+									c,
+									mdport, *iport ? iport : xports,
+									nvram_safe_get("lan_ipaddr"),	// corrected by ipt
+									nvram_safe_get("lan_netmask"),
+									ip,
+									wanfaces.iface[n].ip);
+							}
 						}
 					}
 				}

release/src/router/rc/qos.c
@@ -37,7 +37,7 @@ void ipt_qos(void)
 	unsigned long max;
 	unsigned long prev_max;
 	int gum;
-	char *qface;
+	const char *qface;
 	int sizegroup;
 	int class_flag;
 	int rule_num;
@@ -220,15 +220,7 @@ void ipt_qos(void)
 	}
 	free(buf);
 
-	switch (get_wan_proto()) {
-	case WP_PPTP:
-	case WP_L2TP:
-		qface = ((*manface) && (nvram_get_int("ppp_defgw") == 0)) ? manface : wanface;
-		break;
-	default:
-		qface = wanface;
-		break;
-	}
+	qface = wanfaces.iface[0].name;
 
 	i = nvram_get_int("qos_default");
 	if ((i < 0) || (i > 9)) i = 3;	// "low"
@@ -282,7 +274,6 @@ void start_qos(void)
 {
 	int i;
 	char *buf, *g, *p;
-	char *qface;
 	unsigned int rate;
 	unsigned int ceil;
 	unsigned int bw;
@@ -343,19 +334,6 @@ void start_qos(void)
 		r2q = (bw * 1000) / (8 * 60000) + 1;
 	}
 
-	qface = nvram_safe_get("wan_iface");
-	switch (get_wan_proto()) {
-	case WP_PPTP:
-	case WP_L2TP:
-		if (nvram_get_int("ppp_defgw") == 0) {
-			p = nvram_safe_get("wan_ipaddr");
-			if (*p && strcmp(p, get_wanip()) != 0 && strcmp(p, "0.0.0.0") != 0) {
-				qface = nvram_safe_get("wan_ifname");
-			}
-		}
-		break;
-	}
-
 	fprintf(f,
 		"#!/bin/sh\n"
 		"I=%s\n"
@@ -369,7 +347,7 @@ void start_qos(void)
 		"\ttc qdisc del dev $I root 2>/dev/null\n"
 		"\t$TQA root handle 1: htb default %u r2q %u\n"
 		"\t$TCA parent 1: classid 1:1 htb rate %ukbit ceil %ukbit %s\n",
-			qface,
+			get_wanface(),
 			nvram_get_int("qos_pfifo") ? "pfifo limit 256" : "sfq perturb 10",
 			(nvram_get_int("qos_default") + 1) * 10, r2q,
 			bw, bw, burst_root);

release/src/router/rc/rc.h
@@ -226,14 +226,11 @@ extern void stop_nas(void);
 extern void notify_nas(const char *ifname);
 
 // firewall.c
-extern char wanface[];
-extern char manface[];
+extern wanface_list_t wanfaces;
 extern char lanface[];
 #ifdef TCONFIG_IPV6
 extern char wan6face[];
 #endif
-extern char wanaddr[];
-extern char manaddr[];
 extern char lan_cclass[];
 extern const char *chain_in_accept;
 extern const char *chain_out_drop;

commit 31c84e346de4d8a6eb15940b2f2c0c8b356ba027

libshared: fix for PPPOE wanface name was broken by commit 8a074b70f8a00e38fce53423e87138d2013abe2e

release/src/router/shared/misc.c
@@ -339,7 +339,13 @@ const wanface_list_t *get_wanfaces(void)
 			break;
 		default:
 			ip = (proto == WP_DISABLED) ? "0.0.0.0" : nvram_safe_get("wan_ipaddr");
-			iface = nvram_safe_get("wan_ifname");
+			if (proto == WP_PPPOE) {
+				iface = nvram_safe_get("wan_iface");
+				if (!(*iface)) iface = "ppp+";
+			}
+			else {
+				iface = nvram_safe_get("wan_ifname");
+			}
 			strlcpy(wanfaces.iface[wanfaces.count].ip, ip, sizeof(wanfaces.iface[0].ip));
 			strlcpy(wanfaces.iface[wanfaces.count++].name, iface, IFNAMSIZ);
 			break;