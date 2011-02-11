f057d99efd1d4e09ebe94f97b73d222bc49cac32
Fedor <fedork@ubuntu.(none)>
2011-02-11 00:58:52 -0500

    tomato_ct [K26]: add conntrack_clear proc entry, cleanup

--- release/src-rt/linux/linux-2.6/net/ipv4/netfilter/tomato_ct.c
+++ release/src-rt/linux/linux-2.6/net/ipv4/netfilter/tomato_ct.c
@@ -76,7 +76,7 @@ static int hashdist_read(char *buffer, char **start, off_t offset, int length, i
 #endif
 
 
-static void interate_all(void (*func)(struct nf_conn *, unsigned long), unsigned long data)
+static void iterate_all(void (*func)(struct nf_conn *, unsigned long), unsigned long data)
 {
 	unsigned int i;
 	struct nf_conntrack_tuple_hash *h;
@@ -112,12 +112,12 @@ static int expireearly_write(struct file *file, const char *buffer, unsigned lon
 		if (n < 10) n = 10;
 			else if (n > 86400) n = 86400;
 		
-		interate_all(expireearly, jiffies + (n * HZ));
+		iterate_all(expireearly, jiffies + (n * HZ));
 	}
 
 /*	
 	if ((length > 0) && (buffer[0] == '1')) {
-		interate_all(expireearly, jiffies + (20 * HZ));
+		iterate_all(expireearly, jiffies + (20 * HZ));
 	}
 */
 	
@@ -133,7 +133,15 @@ static void clearmarks(struct nf_conn *ct, unsigned long data)
 static int clearmarks_write(struct file *file, const char *buffer, unsigned long length, void *data)
 {
 	if ((length > 0) && (buffer[0] == '1')) {
-		interate_all(clearmarks, 0);
+		iterate_all(clearmarks, 0);
+	}
+	return length;
+}
+
+static int conntrack_clear_write(struct file *file, const char *buffer, unsigned long length, void *data)
+{
+	if ((length > 0) && (buffer[0] == '1')) {
+		nf_conntrack_flush();
 	}
 	return length;
 }
@@ -158,11 +166,17 @@ static int __init init(void)
 		p->owner = THIS_MODULE;
 		p->write_proc = expireearly_write;
 	}
-	
+
 	p = create_proc_entry("clear_marks", 0200, proc_net);
 	if (p) {
 		p->owner = THIS_MODULE;
 		p->write_proc = clearmarks_write;
+	}
+
+	p = create_proc_entry("conntrack_clear", 0200, proc_net);
+	if (p) {
+		p->owner = THIS_MODULE;
+		p->write_proc = conntrack_clear_write;
 	}
 #endif /* CONFIG_PROC_FS */
 	
@@ -177,6 +191,7 @@ static void __exit fini(void)
 #endif
 	remove_proc_entry("expire_early", proc_net);
 	remove_proc_entry("clear_marks", proc_net);
+	remove_proc_entry("conntrack_clear", proc_net);
 #endif /* CONFIG_PROC_FS */
 }
 
