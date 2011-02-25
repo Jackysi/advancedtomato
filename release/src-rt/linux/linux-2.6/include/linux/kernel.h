dfc23ce13cfc1aa042e4df84fb627f17e8958394
Fedor <fedork@ubuntu.(none)>
2011-02-24 22:06:45 -0500

    [NETFILTER]: {ip,ip6}t_LOG: enhancements from upstream
    kernel.org commits
    17dfc93f6d7e5792c9c36dd70c8612721a091ae8 [NETFILTER]: {ip,ip6}t_LOG: log GID
    36e2a1b0f7f2598e38952494b91490f58aa221c8 [NETFILTER]: {ip,ip6}t_LOG: print MARK value in log output
    f0d57a54aa9fdf3a4d9435d44c69b20388ad0b3b netfilter: ipt_LOG/ip6t_LOG: use more appropriate log level as default
    cf377eb4aeded926375d4d0fe0b66ba95f0521e1 netfilter: ipt_LOG/ip6t_LOG: remove comparison within loop
    7eb9282cd0efac08b8377cbd5037ba297c77e3f7 netfilter: ipt_LOG/ip6t_LOG: add option to print decoded MAC header
    a8defca048fd11eb2d1a17ab61a60a856292dd4e netfilter: ipt_LOG: add bufferisation to call printk() once
    eecc545856c8a0f27783a440d25f4ceaa1f95ce8 netfilter: add missing xt_log.h file
    
    Also backport printk_once() convenience macro

--- release/src-rt/linux/linux-2.6/include/linux/kernel.h
+++ release/src-rt/linux/linux-2.6/include/linux/kernel.h
@@ -157,6 +157,19 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	__attribute__ ((format (printf, 1, 0)));
 asmlinkage int printk(const char * fmt, ...)
 	__attribute__ ((format (printf, 1, 2)));
+
+/*
+ * Print a one-time message (analogous to WARN_ONCE() et al):
+ */
+#define printk_once(x...) ({			\
+	static bool __print_once;		\
+						\
+	if (!__print_once) {			\
+		__print_once = true;		\
+		printk(x);			\
+	}					\
+})
+
 #else
 static inline int vprintk(const char *s, va_list args)
 	__attribute__ ((format (printf, 1, 0)));
@@ -164,6 +177,10 @@ static inline int vprintk(const char *s, va_list args) { return 0; }
 static inline int printk(const char *s, ...)
 	__attribute__ ((format (printf, 1, 2)));
 static inline int printk(const char *s, ...) { return 0; }
+
+/* No effect, but we still get type checking even in the !PRINTK case: */
+#define printk_once(x...) printk(x)
+
 #endif
 
 unsigned long int_sqrt(unsigned long);
