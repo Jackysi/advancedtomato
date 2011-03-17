Jackysi/advancedtomato

commit dfc6fa9e608e9e236cb48d6709bcbaaea458e8e6

Build: use gcc instead of ld to relink shared libraries

release/src/btools/libfoo.pl
@@ -351,7 +351,8 @@ sub genSO
 
 	print LOG "\n\n${base}\n";
 	
-	$cmd = "mipsel-uclibc-ld -shared -s -z combreloc --warn-common --fatal-warnings ${opt} -soname ${name} -o ${so}";
+#	$cmd = "mipsel-uclibc-ld -shared -s -z combreloc --warn-common --fatal-warnings ${opt} -soname ${name} -o ${so}";
+	$cmd = "mipsel-uclibc-gcc -shared -nostdlib -Wl,-s,-z,combreloc -Wl,--warn-common -Wl,--fatal-warnings -Wl,--gc-sections ${opt} -Wl,-soname=${name} -o ${so}";
 	foreach (@{$elf_lib{$name}}) {
 		if ((!$elf_dyn{$name}{$_}) && (/^lib(.+)\.so/)) {
 			$cmd .= " -l$1";
@@ -421,16 +422,11 @@ if ($ARGV[0] eq "--noopt") {
 	$stripshared = "no";
 }
 
-# WAR for a weird "ld" bug: unless we drag in these few functions, re-linking
-# libpthread.so.0 completely screws it up - at least in the current toolchain
-# (binutils 2.20.1 / gcc 4.2.4) :(
-$libpthreadwar = "-u pthread_mutexattr_init -u pthread_mutexattr_settype -u pthread_mutexattr_destroy";
-
-genSO("${root}/lib/libc.so.0", "${uclibc}/lib/libc.a", "${stripshared}", "-init __uClibc_init ${uclibc}/lib/optinfo/interp.os");
+genSO("${root}/lib/libc.so.0", "${uclibc}/lib/libc.a", "${stripshared}", "-Wl,-init=__uClibc_init ${uclibc}/lib/optinfo/interp.os");
 genSO("${root}/lib/libresolv.so.0", "${uclibc}/lib/libresolv.a", "${stripshared}");
 genSO("${root}/lib/libcrypt.so.0", "${uclibc}/lib/libcrypt.a", "${stripshared}");
 genSO("${root}/lib/libm.so.0", "${uclibc}/lib/libm.a", "${stripshared}");
-genSO("${root}/lib/libpthread.so.0", "${uclibc}/lib/libpthread.a", "${stripshared}", "${libpthreadwar}");
+genSO("${root}/lib/libpthread.so.0", "${uclibc}/lib/libpthread.a", "${stripshared}", "-Wl,-init=__pthread_initialize_minimal_internal");
 genSO("${root}/lib/libutil.so.0", "${uclibc}/lib/libutil.a", "${stripshared}");
 #  genSO("${root}/lib/libdl.so.0", "${uclibc}/lib/libdl.a", "${stripshared}");
 #  genSO("${root}/lib/libnsl.so.0", "${uclibc}/lib/libnsl.a", "${stripshared}");

commit 371f24fe388bd2984696afccc463a2fa5a6859cc

Build: fix relinking libpthread.so (again) was broken for K24 builds by commit dfc6fa9e608e9e236cb48d6709bcbaaea458e8e6

release/src/btools/libfoo.pl
@@ -426,7 +426,7 @@ genSO("${root}/lib/libc.so.0", "${uclibc}/lib/libc.a", "${stripshared}", "-Wl,-i
 genSO("${root}/lib/libresolv.so.0", "${uclibc}/lib/libresolv.a", "${stripshared}");
 genSO("${root}/lib/libcrypt.so.0", "${uclibc}/lib/libcrypt.a", "${stripshared}");
 genSO("${root}/lib/libm.so.0", "${uclibc}/lib/libm.a", "${stripshared}");
-genSO("${root}/lib/libpthread.so.0", "${uclibc}/lib/libpthread.a", "${stripshared}", "-Wl,-init=__pthread_initialize_minimal_internal");
+genSO("${root}/lib/libpthread.so.0", "${uclibc}/lib/libpthread.a", "${stripshared}", "-u pthread_mutexattr_init -Wl,-init=__pthread_initialize_minimal_internal");
 genSO("${root}/lib/libutil.so.0", "${uclibc}/lib/libutil.a", "${stripshared}");
 #  genSO("${root}/lib/libdl.so.0", "${uclibc}/lib/libdl.a", "${stripshared}");
 #  genSO("${root}/lib/libnsl.so.0", "${uclibc}/lib/libnsl.a", "${stripshared}");