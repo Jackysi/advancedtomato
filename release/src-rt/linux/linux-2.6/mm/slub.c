d604528e334187d66cd9193ae279848f739a9475
Christoph Lameter <clameter@sgi.com>
2011-04-10 12:56:14 -0400

    mm: categorize GFP flags
    kernel.org commit 6cb062296f73e74768cca2f3eaf90deac54de02d

--- release/src-rt/linux/linux-2.6/mm/slub.c
+++ release/src-rt/linux/linux-2.6/mm/slub.c
@@ -1009,9 +1009,10 @@ static struct page *new_slab(struct kmem_cache *s, gfp_t flags, int node)
 	void *last;
 	void *p;
 
-	BUG_ON(flags & ~(GFP_DMA | __GFP_ZERO | GFP_LEVEL_MASK));
+	BUG_ON(flags & GFP_SLAB_BUG_MASK);
 
-	page = allocate_slab(s, flags & GFP_LEVEL_MASK, node);
+	page = allocate_slab(s,
+		flags & (GFP_RECLAIM_MASK | GFP_CONSTRAINT_MASK), node);
 	if (!page)
 		goto out;
 
