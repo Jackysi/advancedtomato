2c0f1e9108bc64cdfc8a1efe48a1d70eb97daea2
Shibby <shibby@openlinksys.info>
2011-05-14 18:21:44 +0200

    added missed files

--- release/src/router/libnfsidmap/nss.c
+++ release/src/router/libnfsidmap/nss.c
@@ -324,9 +324,9 @@ int nss_gss_princ_to_grouplist(char *secname, char *princ,
 		ret = -ENOENT;
 		goto out;
 	}
-	if (getgrouplist(pw->pw_name, pw->pw_gid, groups, ngroups) < 0)
-		ret = -ERANGE;
-	free(pw);
+	//	if (getgrouplist(pw->pw_name, pw->pw_gid, groups, ngroups) < 0)
+	//		ret = -ERANGE;
+	//	free(pw);
 out:
 	return ret;
 }
