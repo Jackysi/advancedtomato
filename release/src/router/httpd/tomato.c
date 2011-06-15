9f1e8606dba9a4c8e276f2b898543241084462ec
Shibby <shibby@openlinksys.info>
2011-06-16 01:21:58 +0700

    Allow turn on/off sd-idle via GUI

--- release/src/router/httpd/tomato.c
+++ release/src/router/httpd/tomato.c
@@ -879,6 +879,7 @@ static const nvset_t nvset_list[] = {
 	{ "script_usbhotplug", 		V_TEXT(0, 2048)			},
 	{ "script_usbmount", 		V_TEXT(0, 2048)			},
 	{ "script_usbumount", 		V_TEXT(0, 2048)			},
+	{ "idle_enable",		V_01				},
 #endif
 
 // nas-ftp - !!TB
@@ -965,7 +966,7 @@ static const nvset_t nvset_list[] = {
 	{ "qosl_dudp",                    V_RANGE(0, 100)        },
 	/*qosl_ibw unused - qos_ibw shared*/
 	/*qosl_obw unused - qos_obw shared*/
-	
+
 // arpbind
 	{ "arpbind_enable",    	 	V_01                    },
 	{ "arpbind_only",       	V_01                   	},
