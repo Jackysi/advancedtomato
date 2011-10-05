97c57aef3857f050f78f29a270d838b0ec208469
Toastman <toastman@galactic_core>
2011-10-05 22:18:28 +0700

    remove duplicated code

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -1978,7 +1978,6 @@ void start_services(void)
 	start_cron();
 //	start_upnp();
 	start_rstats(0);
-	start_account();
 	start_cstats(0);
 	start_sched();
 #ifdef TCONFIG_IPV6
@@ -2014,6 +2013,7 @@ void stop_services(void)
 #endif
 	stop_sched();
 	stop_rstats();
+	start_account();
 	stop_cstats();
 //	stop_upnp();
 	stop_cron();
@@ -2092,18 +2092,6 @@ TOP:
 		goto CLEAR;
 	}
 
-	if (strcmp(service, "account") == 0) {
-		if (action & A_STOP) stop_account();
-		if (action & A_START) start_account();
-		goto CLEAR;
-	}
-
-	if (strcmp(service, "arpbind") == 0) {
-		if (action & A_STOP) stop_arpbind();
-		if (action & A_START) start_arpbind();
-		goto CLEAR;
-	}
-
 	if (strcmp(service, "restrict") == 0) {
 		if (action & A_STOP) {
 			stop_firewall();
@@ -2126,12 +2114,6 @@ TOP:
 	if (strcmp(service, "account") == 0) {
 		if (action & A_STOP) stop_account();
 		if (action & A_START) start_account();
-		goto CLEAR;
-	}
-
-	if (strcmp(service, "arpbind") == 0) {
-		if (action & A_STOP) stop_arpbind();
-		if (action & A_START) start_arpbind();
 		goto CLEAR;
 	}
 
