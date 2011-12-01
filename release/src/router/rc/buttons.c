9e2ccd388c0c032b6bb189244216da22c6e767c9
Shibby <shibby@openlinksys.info>
2011-12-01 13:52:37 +0100

    Support Asus RT-N10U

--- release/src/router/rc/buttons.c
+++ release/src/router/rc/buttons.c
@@ -141,6 +141,11 @@ int buttons_main(int argc, char *argv[])
 		reset_mask = 1 << 3;
 		ses_mask = 1 << 2;
 		break;
+	case MODEL_RTN10U:
+		reset_mask = 1 << 21;
+		ses_mask = 1 << 20;
+		ses_led = LED_AOSS;
+		break;
 	case MODEL_RTN12:
 		reset_mask = 1 << 1;
 		ses_mask = 1 << 0;
