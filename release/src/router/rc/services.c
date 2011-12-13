1c2ac93d9f8f8a87627ba29cff3451024d2624c3
Augusto Bott <augusto@bott.com.br>
2011-12-13 12:36:55 -0200

    cstats/rstats: allow more time between SIGTERMs at shutdown
    * Don't hit rstats/cstats so often with SIGTERM as this might cause gzip to fail in the middle, corrupting the datafile being saved at shutdown.

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -1315,10 +1315,10 @@ static void stop_rstats(void)
 	int n;
 	int pid;
 
-	n = 60;
+	n = 20;
 	while ((n-- > 0) && ((pid = pidof("rstats")) > 0)) {
 		if (kill(pid, SIGTERM) != 0) break;
-		sleep(1);
+		sleep(3);
 	}
 }
 
@@ -1336,10 +1336,10 @@ static void stop_cstats(void)
 	int n;
 	int pid;
 
-	n = 60;
+	n = 20;
 	while ((n-- > 0) && ((pid = pidof("cstats")) > 0)) {
 		if (kill(pid, SIGTERM) != 0) break;
-		sleep(1);
+		sleep(3);
 	}
 }
 
