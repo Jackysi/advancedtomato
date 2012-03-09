88249c0b290d936b9602c178cda37ba7490c0b52
Shibby <shibby@openlinksys.info>
2012-03-09 09:20:46 +0100

    fix compilation

--- release/src/router/httpd/misc.c
+++ release/src/router/httpd/misc.c
@@ -784,7 +784,7 @@ void wo_wakeup(char *url)
 				eval("ether-wake", "-b", "-i", nvram_safe_get("lan2_ifname"), mac);
 			if (strcmp(nvram_safe_get("lan3_ifname"), "") != 0)
 				eval("ether-wake", "-b", "-i", nvram_safe_get("lan3_ifname"), mac);
-			}
+
 			mac = p + 1;
 		}
 	}
