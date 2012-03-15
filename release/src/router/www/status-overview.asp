Jackysi/advancedtomato

commit 2e03d9abe2830af4ebeb20360a60664e58c80d84

WebUI: cosmetics

release/src/router/www/status-overview.asp
@@ -156,16 +156,38 @@ function earlyInit()
 {
 	elem.display('b_dhcpc', show_dhcpc);
 	elem.display('b_connect', 'b_disconnect', show_codi);
-	elem.display('wan-title', 'wan-section', nvram.wan_proto != 'disabled');
+	if (nvram.wan_proto == 'disabled')
+		elem.display('wan-title', 'sesdiv_wan', 0);
 	for (var uidx = 0; uidx < wl_ifaces.length; ++uidx) {
-		elem.display('b_wl'+uidx+'_enable', 'b_wl'+uidx+'_disable', show_radio[uidx]);
+		elem.display('b_wl'+wl_fface(uidx)+'_enable', 'b_wl'+wl_fface(uidx)+'_disable', show_radio[uidx]);
 	}
 	show();
 }
 
 function init()
 {
+	var c;
+	if (((c = cookie.get('status_overview_system_vis')) != null) && (c != '1')) toggleVisibility("system");
+	if (((c = cookie.get('status_overview_wan_vis')) != null) && (c != '1')) toggleVisibility("wan");
+	if (((c = cookie.get('status_overview_lan_vis')) != null) && (c != '1')) toggleVisibility("lan");
+	for (var uidx = 0; uidx < wl_ifaces.length; ++uidx) {
+		u = wl_fface(uidx);
+		if (((c = cookie.get('status_overview_wl_'+u+'_vis')) != null) && (c != '1')) toggleVisibility("wl_"+u);
+	}
+
 	ref.initPage(3000, 3);
+}
+
+function toggleVisibility(whichone) {
+	if (E('sesdiv_' + whichone).style.display == '') {
+		E('sesdiv_' + whichone).style.display = 'none';
+		E('sesdiv_' + whichone + '_showhide').innerHTML = '(Click here to show)';
+		cookie.set('status_overview_' + whichone + '_vis', 0);
+	} else {
+		E('sesdiv_' + whichone).style.display='';
+		E('sesdiv_' + whichone + '_showhide').innerHTML = '(Click here to hide)';
+		cookie.set('status_overview_' + whichone + '_vis', 1);
+	}
 }
 </script>
 
@@ -183,8 +205,8 @@ function init()
 
 <!-- / / / -->
 
-<div class='section-title'>System</div>
-<div class='section'>
+<div class='section-title'>System <small><i><a href='javascript:toggleVisibility("system");'><span id='sesdiv_system_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section' id='sesdiv_system'>
 <script type='text/javascript'>
 createFieldTable('', [
 	{ title: 'Name', text: nvram.router_name },
@@ -203,8 +225,8 @@ createFieldTable('', [
 </script>
 </div>
 
-<div class='section-title' id='wan-title'>WAN</div>
-<div class='section' id='wan-section'>
+<div class='section-title' id='wan-title'>WAN <small><i><a href='javascript:toggleVisibility("wan");'><span id='sesdiv_wan_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section' id='sesdiv_wan'>
 <script type='text/javascript'>
 createFieldTable('', [
 	{ title: 'MAC Address', text: nvram.wan_hwaddr },
@@ -232,8 +254,8 @@ createFieldTable('', [
 </div>
 
 
-<div class='section-title'>LAN</div>
-<div class='section'>
+<div class='section-title'>LAN <small><i><a href='javascript:toggleVisibility("lan");'><span id='sesdiv_lan_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section' id='sesdiv_lan'>
 <script type='text/javascript'>
 
 function h_countbitsfromleft(num) {
@@ -303,11 +325,12 @@ createFieldTable('', [
 <script type='text/javascript'>
 for (var uidx = 0; uidx < wl_ifaces.length; ++uidx) {
 u = wl_unit(uidx);
-W('<div class=\'section-title\' id=\'wl'+uidx+'-title\'>Wireless');
-if (wl_ifaces.length > 1)
+W('<div class=\'section-title\' id=\'wl'+u+'-title\'>Wireless');
+if (wl_ifaces.length > 0)
 	W(' (' + wl_display_ifname(uidx) + ')');
+W(' <small><i><a href=\'javascript:toggleVisibility("wl_' + u + '");\'><span id=\'sesdiv_wl_' +u + '_showhide\'>(Click here to hide)</span></a></i></small>');
 W('</div>');
-W('<div class=\'section\' id=\'wl'+uidx+'-section\'>');
+W('<div class=\'section\' id=\'sesdiv_wl_'+u+'\'>');
 sec = auth[nvram['wl'+u+'_security_mode']] + '';
 if (sec.indexOf('WPA') != -1) sec += ' + ' + enc[nvram['wl'+u+'_crypto']];
 

commit f742331c43b90bab8135e18bd9fb906389a91624

fix status-overview page
* fix autorefresh
* re-add enable/disable wireless buttons
* some cosmetics changes

broken by commit 2e03d9abe2830af4ebeb20360a60664e58c80d84

release/src/router/www/status-overview.asp
@@ -159,7 +159,7 @@ function earlyInit()
 	if (nvram.wan_proto == 'disabled')
 		elem.display('wan-title', 'sesdiv_wan', 0);
 	for (var uidx = 0; uidx < wl_ifaces.length; ++uidx) {
-		elem.display('b_wl'+wl_fface(uidx)+'_enable', 'b_wl'+wl_fface(uidx)+'_disable', show_radio[uidx]);
+		elem.display('b_wl'+uidx+'_enable', 'b_wl'+uidx+'_disable', show_radio[uidx]);
 	}
 	show();
 }
@@ -171,7 +171,7 @@ function init()
 	if (((c = cookie.get('status_overview_wan_vis')) != null) && (c != '1')) toggleVisibility("wan");
 	if (((c = cookie.get('status_overview_lan_vis')) != null) && (c != '1')) toggleVisibility("lan");
 	for (var uidx = 0; uidx < wl_ifaces.length; ++uidx) {
-		u = wl_fface(uidx);
+		u = wl_unit(uidx);
 		if (((c = cookie.get('status_overview_wl_'+u+'_vis')) != null) && (c != '1')) toggleVisibility("wl_"+u);
 	}
 
@@ -181,11 +181,11 @@ function init()
 function toggleVisibility(whichone) {
 	if (E('sesdiv_' + whichone).style.display == '') {
 		E('sesdiv_' + whichone).style.display = 'none';
-		E('sesdiv_' + whichone + '_showhide').innerHTML = '(Click here to show)';
+		E('sesdiv_' + whichone + '_showhide').innerHTML = '(show)';
 		cookie.set('status_overview_' + whichone + '_vis', 0);
 	} else {
 		E('sesdiv_' + whichone).style.display='';
-		E('sesdiv_' + whichone + '_showhide').innerHTML = '(Click here to hide)';
+		E('sesdiv_' + whichone + '_showhide').innerHTML = '(hide)';
 		cookie.set('status_overview_' + whichone + '_vis', 1);
 	}
 }
@@ -205,7 +205,7 @@ function toggleVisibility(whichone) {
 
 <!-- / / / -->
 
-<div class='section-title'>System <small><i><a href='javascript:toggleVisibility("system");'><span id='sesdiv_system_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section-title'>System <small><i><a href='javascript:toggleVisibility("system");'><span id='sesdiv_system_showhide'>(hide)</span></a></i></small></div>
 <div class='section' id='sesdiv_system'>
 <script type='text/javascript'>
 createFieldTable('', [
@@ -225,7 +225,7 @@ createFieldTable('', [
 </script>
 </div>
 
-<div class='section-title' id='wan-title'>WAN <small><i><a href='javascript:toggleVisibility("wan");'><span id='sesdiv_wan_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section-title' id='wan-title'>WAN <small><i><a href='javascript:toggleVisibility("wan");'><span id='sesdiv_wan_showhide'>(hide)</span></a></i></small></div>
 <div class='section' id='sesdiv_wan'>
 <script type='text/javascript'>
 createFieldTable('', [
@@ -254,7 +254,7 @@ createFieldTable('', [
 </div>
 
 
-<div class='section-title'>LAN <small><i><a href='javascript:toggleVisibility("lan");'><span id='sesdiv_lan_showhide'>(Click here to hide)</span></a></i></small></div>
+<div class='section-title'>LAN <small><i><a href='javascript:toggleVisibility("lan");'><span id='sesdiv_lan_showhide'>(hide)</span></a></i></small></div>
 <div class='section' id='sesdiv_lan'>
 <script type='text/javascript'>
 
@@ -328,7 +328,7 @@ u = wl_unit(uidx);
 W('<div class=\'section-title\' id=\'wl'+u+'-title\'>Wireless');
 if (wl_ifaces.length > 0)
 	W(' (' + wl_display_ifname(uidx) + ')');
-W(' <small><i><a href=\'javascript:toggleVisibility("wl_' + u + '");\'><span id=\'sesdiv_wl_' +u + '_showhide\'>(Click here to hide)</span></a></i></small>');
+W(' <small><i><a href=\'javascript:toggleVisibility("wl_' + u + '");\'><span id=\'sesdiv_wl_' +u + '_showhide\'>(hide)</span></a></i></small>');
 W('</div>');
 W('<div class=\'section\' id=\'sesdiv_wl_'+u+'\'>');
 sec = auth[nvram['wl'+u+'_security_mode']] + '';