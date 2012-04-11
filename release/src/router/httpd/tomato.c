1e0e835b00038aade91fad42c42d359bab0a0dab
Augusto Bott <augusto@bott.com.br>
2012-04-11 03:41:31 -0300

    EXPERIMENTAL VLAN/VID mapping GUI
    * users can now override the default behaviour of VLAN ID assignments
    * allows creating VLANs with 802.1Q tags larger than 15 via GUI (up to 4k, even on old-ish devices)s
    * extends features on the 'vlan0tag' series of commits by Fedor and lly
    * user can set a 'First 802.1Q VLAN tag' to be used (initial offset for VIDs, must be multiple of 16 or zero/unset)
    * see commits ccc88db8be972dc19d2098258d1ee065bd0f5a42 and cdb1732b4152c468cca0e9debf0bd46e380cc6ca
    * this commit also includes some patches from DD-WRT
    * http://www.dd-wrt.com/phpBB2/viewtopic.php?t=86919
    * http://www.freqoflife.com/technology/linksys-wrt54gl-vlan-tagging/
    * some restrictions for VID assignments may apply (hardware-dependent)

--- release/src/router/httpd/tomato.c
+++ release/src/router/httpd/tomato.c
@@ -748,6 +748,23 @@ static const nvset_t nvset_list[] = {
 	{ "lan_ifnames",		V_TEXT(0,64)			},
 	{ "manual_boot_nv",		V_01				},
 	{ "trunk_vlan_so",		V_01				},
+	{ "vlan0tag",		V_TEXT(0,5)			},
+	{ "vlan0vid",		V_TEXT(0,5)			},
+	{ "vlan1vid",		V_TEXT(0,5)			},
+	{ "vlan2vid",		V_TEXT(0,5)			},
+	{ "vlan3vid",		V_TEXT(0,5)			},
+	{ "vlan4vid",		V_TEXT(0,5)			},
+	{ "vlan5vid",		V_TEXT(0,5)			},
+	{ "vlan6vid",		V_TEXT(0,5)			},
+	{ "vlan7vid",		V_TEXT(0,5)			},
+	{ "vlan8vid",		V_TEXT(0,5)			},
+	{ "vlan9vid",		V_TEXT(0,5)			},
+	{ "vlan10vid",		V_TEXT(0,5)			},
+	{ "vlan11vid",		V_TEXT(0,5)			},
+	{ "vlan12vid",		V_TEXT(0,5)			},
+	{ "vlan13vid",		V_TEXT(0,5)			},
+	{ "vlan14vid",		V_TEXT(0,5)			},
+	{ "vlan15vid",		V_TEXT(0,5)			},
 #endif
 
 // advanced-mac
