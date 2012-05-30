Jackysi/advancedtomato

commit ce6d38c0bbb8228e590476eec034faf9c929d1fe

Add new Linksys E-series routers support
Big thanks for @tvlz from linksysinfo.org !

This commit add support for:
 - E1000v2/2.1
 - Cisco M10v2
 - E900
 - E1200v1
 - E1200v2
 - E1500
 - E1550
 - E2500
 - E3200

Thread: http://www.linksysinfo.org/index.php?threads/adding-linksys-e-series-support.37444/#post-183837

release/src/router/www/advanced-vlan.asp
@@ -96,6 +96,26 @@ switch(nvram['boardtype']) {
     COL_P3N = '1';
     COL_P4N = '0';
     break;
+  case '0xf53a':  // E1000v2.1/E1200v1
+  case '0xf53b':   // E1000v2/E1500
+   if (nvram['boot_hw_model'] == 'E1200' && nvram['boot_hw_ver'] == '1.0') || (nvram['boot_hw_model'] == 'E1500') {
+     COL_P0N = '0';
+     COL_P1N = '1';
+     COL_P2N = '2';
+     COL_P3N = '3';
+     COL_P4N = '4';
+   break;
+   }
+   COL_P0N = '1';
+   COL_P1N = '2';
+   COL_P2N = '3';
+   COL_P3N = '4';
+   COL_P4N = '0';
+   break;
+  case '0xc550':  // E1550
+  case '0xf550':  // E2500
+  case '0x058e':  // E900
+  case '0xf52a':  // E3200
   case '0xf52c':  // E4200
     COL_P0N = '0';
     COL_P1N = '1';

release/src-rt/shared/bcmrobo.c
@@ -1129,7 +1129,7 @@ bcm_robo_config_vlan(robo_info_t *robo, uint8 *mac_addr)
 			/* Set the Static bit , Valid bit and Port ID fields in
 			 * ARL Table Data Entry 0 Register
 			 */
-			if (robo->devid == DEVID53115) {
+			if ((robo->devid == DEVID53115) || (robo->devid == DEVID53125)) {
 				val32 = 0x18008;
 				robo->ops->write_reg(robo, PAGE_VTBL, REG_VTBL_DAT_E0,
 				                     &val32, sizeof(val32));
@@ -1233,7 +1233,7 @@ vlan_setup:
 		robo->ops->write_reg(robo, PAGE_VLAN, REG_VLAN_PMAP, &val32, sizeof(val32));
 	}
 
-	if (robo->devid == DEVID53115) {
+	if ((robo->devid == DEVID53115) || (robo->devid == DEVID53125)) {
 		/* Configure the priority system to use to determine the TC of
 		 * ingress frames. Use DiffServ TC mapping, otherwise 802.1p
 		 * TC mapping, otherwise MAC based TC mapping.
@@ -1450,7 +1450,7 @@ robo_power_save_mode_clear_auto(robo_info_t *robo, int32 phy)
 {
 	uint16 val16;
 
-	if (robo->devid == DEVID53115) {
+	if ((robo->devid == DEVID53115) || (robo->devid == DEVID53125)) {
 		/* For 53115 0x1C is the MII address of the auto power
 		 * down register. Bit 5 is enabling the mode
 		 * bits has the following purpose
@@ -1497,7 +1497,7 @@ robo_power_save_mode_clear_manual(robo_info_t *robo, int32 phy)
 	uint8 val8;
 	uint16 val16;
 
-	if ((robo->devid == DEVID53115) ||
+	if ((robo->devid == DEVID53115) || (robo->devid == DEVID53125) ||
 	    (robo->sih->chip == BCM5356_CHIP_ID)) {
 		/* For 53115 0x0 is the MII control register
 		 * Bit 11 is the power down mode bit 
@@ -1610,7 +1610,7 @@ robo_power_save_mode_auto(robo_info_t *robo, int32 phy)
 	uint16 val16;
 
 	/* If the switch supports auto power down enable that */  
-	if (robo->devid ==  DEVID53115) {
+	if ((robo->devid ==  DEVID53115) || (robo->devid == DEVID53125)) {
 		/* For 53115 0x1C is the MII address of the auto power
 		 * down register. Bit 5 is enabling the mode 	
 		 * bits has the following purpose
@@ -1665,7 +1665,7 @@ robo_power_save_mode_manual(robo_info_t *robo, int32 phy)
 		return 0;
 
 	/* If the switch supports manual power down enable that */  
-	if ((robo->devid ==  DEVID53115) ||
+	if ((robo->devid ==  DEVID53115) || (robo->devid == DEVID53125) ||
 	    (robo->sih->chip == BCM5356_CHIP_ID)) {
 		/* For 53115 0x0 is the MII control register bit 11 is the
 		 * power down mode bit 
@@ -1806,7 +1806,7 @@ robo_plc_hw_init(robo_info_t *robo)
 	if (robo->ops->enable_mgmtif)
 		robo->ops->enable_mgmtif(robo);
 
-	if (robo->devid == DEVID53115) {
+	if ((robo->devid == DEVID53115) || (robo->devid == DEVID53125)) {
 		/* Fix the duplex mode and speed for Port 5 */
 		val8 = ((1 << 6) | (1 << 2) | 3);
 		robo->ops->write_reg(robo, PAGE_CTRL, REG_CTRL_MIIP5O, &val8, sizeof(val8));

release/src/Makefile
@@ -130,7 +130,17 @@ ifneq ($(MIPS32),r2)
 endif
 endif
 ifeq ($(LINKSYS_E),y)
-	@rm -f image/tomato-E??00$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
+	@rm -f image/tomato-E??????$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
+endif
+ifeq ($(LINKSYS_E_64k),y)
+	@rm -f image/tomato-E??????$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
+endif
+ifeq ($(LINKSYS_E1200v1),y)
+	@rm -f image/tomato-E1200v1$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
+endif
+ifeq ($(LINKSYS_E1000v2),y)
+	@rm -f image/tomato-E1000v2-v21$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
+	@rm -f image/tomato-Cisco-M10v2$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin
 endif
 ifeq ($(BELKIN),y)
 ifneq ($(NVRAM_SIZE),60)
@@ -155,11 +165,35 @@ ifneq ($(MIPS32),r2)
 endif
 endif
 ifeq ($(LINKSYS_E),y)
-	# Linksys E-series images
+	# Linksys E-series(60k Nvram) images
 	@btools/fpkg -i lzma-loader/loader.gz -i $(LINUXDIR)/arch/mips/brcm-boards/bcm947xx/compressed/vmlinuz -a 1024 -i router/mipsel-uclibc/target.image \
+		-l 1550,image/tomato-E1550$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		-l E25X,image/tomato-E2500$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		-l 3200,image/tomato-E3200$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
 		-l 4200,image/tomato-E4200$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
 		-l 61XN,image/tomato-E3000$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
 		-l 32XN,image/tomato-E2000$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		| tee -a fpkg.log
+endif
+ifeq ($(LINKSYS_E_64k),y)
+	# Linksys E-series(64k Nvram) images
+	@btools/fpkg -i lzma-loader/loader.gz -i $(LINUXDIR)/arch/mips/brcm-boards/bcm947xx/compressed/vmlinuz -a 1024 -i router/mipsel-uclibc/target.image \
+		-l E900,image/tomato-E900$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		-l E122,image/tomato-E1200v2$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		-l E150,image/tomato-E1500$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		| tee -a fpkg.log
+endif
+ifeq ($(LINKSYS_E1000v2),y)
+	# Linksys E1000v2/v2.1 images
+	@btools/fpkg -i lzma-loader/loader.gz -i $(LINUXDIR)/arch/mips/brcm-boards/bcm947xx/compressed/vmlinuz -a 1024 -i router/mipsel-uclibc/target.image \
+		-l E100,image/tomato-E1000v2-v21$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		-l M010,image/tomato-Cisco-M10v2$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
+		| tee -a fpkg.log
+endif
+ifeq ($(LINKSYS_E1200v1),y)
+	# Linksys E1200v1 images
+	@btools/fpkg -i lzma-loader/loader.gz -i $(LINUXDIR)/arch/mips/brcm-boards/bcm947xx/compressed/vmlinuz -a 1024 -i router/mipsel-uclibc/target.image \
+		-l E120,image/tomato-E1200v1$(current_BUILD_USB)$(if $(filter $(NVRAM_SIZE),0),,-NVRAM$(NVRAM_SIZE)K)-$(current_TOMATO_VER).$(V1)$(mips_rev)$(beta)$(V2)-$(current_BUILD_DESC).bin \
 		| tee -a fpkg.log
 endif
 ifeq ($(BELKIN),y)
@@ -817,7 +851,7 @@ r2i:
 r2v:
 	@$(MAKE) v MIPS32=r2
 
-#AIO for E4200
+#Linksys E-series witk 60K NVRAM builds
 n60z:
 	@$(MAKE) r2z NVRAM_SIZE=60 LINKSYS_E=y
 
@@ -825,7 +859,7 @@ n60c:
 	@$(MAKE) r2c NVRAM_SIZE=60 LINKSYS_E=y
 
 n60u:
-	@$(MAKE) r2u NVRAM_SIZE=60 LINKSYS_E=1
+	@$(MAKE) r2u NVRAM_SIZE=60 LINKSYS_E=y
 
 n60t:
 	@$(MAKE) r2t NVRAM_SIZE=60 LINKSYS_E=y
@@ -834,7 +868,7 @@ n60b:
 	@$(MAKE) r2b NVRAM_SIZE=60 LINKSYS_E=y
 
 n60d:
-	@$(MAKE) r2d NVRAM_SIZE=60 LINKSYS_E=1
+	@$(MAKE) r2d NVRAM_SIZE=60 LINKSYS_E=y
 
 n60o:
 	@$(MAKE) r2o NVRAM_SIZE=60 LINKSYS_E=y
@@ -843,7 +877,50 @@ n60k:
 	@$(MAKE) r2k NVRAM_SIZE=60 LINKSYS_E=y
 
 n60v:
-	@$(MAKE) r2v NVRAM_SIZE=60 LINKSYS_E=1
+	@$(MAKE) r2v NVRAM_SIZE=60 LINKSYS_E=y
+
+
+#Linksys E-Series with 64K Nvram Builds
+
+n64z:
+	@$(MAKE) r2z NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64c:
+	@$(MAKE) r2c NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64u:
+	@$(MAKE) r2u NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64t:
+	@$(MAKE) r2t NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64b:
+	@$(MAKE) r2b NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64d:
+	@$(MAKE) r2d NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64o:
+	@$(MAKE) r2o NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+n64k:
+	@$(MAKE) r2k NVRAM_SIZE=64 LINKSYS_E_64k=y
+
+
+#Linksys E-Series with 4MB Flash
+
+e1000v2i:
+	@$(MAKE) r2i NVRAM_SIZE=32 LINKSYS_E1000v2=y
+
+e1000v2f:
+	@$(MAKE) r2f NVRAM_SIZE=32 LINKSYS_E1000v2=y
+
+e1200v1i:
+	@$(MAKE) r2i NVRAM_SIZE=64 LINKSYS_E1200v1=y
+
+e1200v1f:
+	@$(MAKE) r2f NVRAM_SIZE=64 LINKSYS_E1200v1=y
+
 
 #Builds for WNR3500L v2
 v2z:
@@ -933,6 +1010,12 @@ ifeq ($(CONFIG_LINUX26),y)
 	@echo "r2v          MIPS Release 2 VPN (no usb)"
 	@echo "r2i          MIPS Release 2 MiniIPv6 (for 4MB flash)"
 	@echo "r2f          MIPS Release 2 Mini (for netgear)"
+	@echo "       Linksys E-Series with 4MB Flash(E1000v2/v2.1/E1200v1)"
+	@echo "e1000v2i     Linksys E1000v2-v2.1/Cisco M10v2 MiniIPv6"
+	@echo "e1000v2f     Linksys E1000v2-v2.1/Cisco M10v2 Mini"
+	@echo "e1200v1i     Linksys E1200v1 MiniIPv6"
+	@echo "e1200v1i     Linksys E1200v1 Mini"
+	@echo "       Linksys E-Series with 60k Nvram"
 	@echo "n60e         Linksys E-series build VPN"
 	@echo "n60c         Linksys E-series build BTGui-VPN"
 	@echo "n60d         Linksys E-series build BTGui-VPN plus Nocat"
@@ -942,6 +1025,15 @@ ifeq ($(CONFIG_LINUX26),y)
 	@echo "n60o         Linksys E-series build Mega-VPN"
 	@echo "n60z         Linksys E-series build AIO"
 	@echo "n60v         Linksys E-series build IPv6-VPN for E2000"
+	@echo "       Linksys E-Series with 64k Nvram/8MB Flash(E900/E1200v2/E1500)"
+	@echo "n64e         Linksys E-series(64k Nvram) build VPN"
+	@echo "n64c         Linksys E-series(64k Nvram) build BTGui-VPN"
+	@echo "n64d         Linksys E-series(64k Nvram) build BTGui-VPN plus Nocat"
+	@echo "n64u         Linksys E-series(64k Nvram) build BT"
+	@echo "n64t         Linksys E-series(64k Nvram) build BT-VPN"
+	@echo "n64b         Linksys E-series(64k Nvram) build Big-VPN"
+	@echo "n64o         Linksys E-series(64k Nvram) build Mega-VPN"
+	@echo "       Netgear WNR3500L v2 builds (128MB Flash)"
 	@echo "v2e          Netgear WNR3500Lv2 build VPN"
 	@echo "v2t          Netgear WNR3500Lv2 build BT-VPN"
 	@echo "v2z          Netgear WNR3500Lv2 build AIO"

release/src/btools/fpkg.c
@@ -320,7 +320,10 @@ void create_cytan(const char *fname, const char *pattern)
 	memset(&h, 0, sizeof(h));
 	memcpy(h.magic, pattern, 4);
 	memcpy(h.u2nd, "U2ND", 4);
-	h.version[0] = 4;		// 4.0.0	should be >= *_VERSION_FROM defined in code_pattern.h
+	h.version[0] = 1;   // stock fw has version check
+	h.version[1] = 1;
+	h.version[2] = 99;
+//	h.version[0] = 4;		// 4.0.0	should be >= *_VERSION_FROM defined in code_pattern.h
 	h.flags = 0xFF;
 	tm = localtime(&max_time);
 	h.date[0] = tm->tm_year - 100;

release/src/router/rc/buttons.c
@@ -187,6 +187,18 @@ int buttons_main(int argc, char *argv[])
 		ses_mask = 1 << 8;
 		ses_led = LED_AOSS;
 		break;
+	case MODEL_E900:
+	case MODEL_E1000v2:
+	case MODEL_E1500:
+	case MODEL_E1550:
+	case MODEL_E2500:
+		reset_mask = 1 << 10;
+		ses_mask = 1 << 9;
+		break;
+	case MODEL_E3200:
+		reset_mask = 1 << 5;
+		ses_mask = 1 << 8;
+		break;
 	case MODEL_WRT160Nv3:
 		reset_mask = 1 << 6;
 		ses_mask = 1 << 5;

release/src/router/rc/init.c
@@ -420,6 +420,7 @@ static int init_vlan_ports(void)
 	case MODEL_RTN10:
 		dirty |= check_nv("vlan1ports", "4 5");
 		break;
+	case MODEL_E1000v2:
 	case MODEL_RTN10U:
 		dirty |= check_nv("vlan0ports", "1 2 3 4 5*");
 		dirty |= check_nv("vlan1ports", "0 5");
@@ -439,12 +440,17 @@ static int init_vlan_ports(void)
 		dirty |= check_nv("vlan1ports", "3 2 1 0 8*");
 		dirty |= check_nv("vlan2ports", "4 8");
 		break;
+	case MODEL_E900:
+	case MODEL_E1500:
+	case MODEL_E2500:
 	case MODEL_F7D3302:
 	case MODEL_F7D4302:
 		dirty |= check_nv("vlan1ports", "0 1 2 3 5*");
 		dirty |= check_nv("vlan2ports", "4 5");
 		break;
 	case MODEL_RTN15U:
+	case MODEL_E1550:
+	case MODEL_E3200:
 	case MODEL_E4200:
 		dirty |= check_nv("vlan1ports", "0 1 2 3 8*");
 		dirty |= check_nv("vlan2ports", "4 8");
@@ -592,6 +598,12 @@ static void check_bootnv(void)
 			dirty |= check_nv("pci/1/1/macaddr", mac);
 		}
 		break;
+	case MODEL_E900:
+	case MODEL_E1000v2:
+	case MODEL_E1500:
+	case MODEL_E1550:
+	case MODEL_E2500:
+	case MODEL_E3200:
 	case MODEL_WRT160Nv3:
 		dirty |= check_nv("vlan2hwname", "et0");
 		break;
@@ -692,6 +704,7 @@ static int init_nvram(void)
 	int model;
 	const char *mfr;
 	const char *name;
+	const char *ver;
 	char s[256];
 	unsigned long bf;
 	unsigned long n;
@@ -1117,12 +1130,69 @@ static int init_nvram(void)
 			nvram_set("wandevs", "vlan2");
 		}
 		break;
+	case MODEL_E900:
+	case MODEL_E1500:
+		mfr = "Linksys";
+		name = nvram_safe_get("boot_hw_model");
+		ver = nvram_safe_get("boot_hw_ver");
+		features = SUP_SES | SUP_80211N;
+		if (!nvram_match("t_fix1", (char *)name)) {
+			nvram_set("lan_ifnames", "vlan1 eth1");
+			nvram_set("wan_ifnameX", "vlan2");
+			nvram_set("wl_ifname", "eth1");
+		}
+		break;
+	case MODEL_E1550:
+		mfr = "Linksys";
+		name = nvram_safe_get("boot_hw_model");
+		ver = nvram_safe_get("boot_hw_ver");
+		features = SUP_SES | SUP_80211N;
+#ifdef TCONFIG_USB
+		nvram_set("usb_uhci", "-1");
+#endif
+		if (!nvram_match("t_fix1", (char *)name)) {
+			nvram_set("lan_ifnames", "vlan1 eth1");
+			nvram_set("wan_ifnameX", "vlan2");
+			nvram_set("wl_ifname", "eth1");
+		}
+		break;
+	case MODEL_E2500:
+		mfr = "Linksys";
+		name = nvram_safe_get("boot_hw_model");
+		ver = nvram_safe_get("boot_hw_ver");
+		features = SUP_SES | SUP_80211N;
+		if (!nvram_match("t_fix1", (char *)name)) {
+			nvram_set("lan_ifnames", "vlan1 eth1 eth2");
+			nvram_set("wan_ifnameX", "vlan2");
+			nvram_set("wl_ifname", "eth1");
+		}
+		break;
+	case MODEL_E3200:
+		mfr = "Linksys";
+		name = nvram_safe_get("boot_hw_model");
+		ver = nvram_safe_get("boot_hw_ver");
+		features = SUP_SES | SUP_80211N | SUP_1000ET;
+#ifdef TCONFIG_USB
+		nvram_set("usb_uhci", "-1");
+#endif
+		if (!nvram_match("t_fix1", (char *)name)) {
+			nvram_set("lan_ifnames", "vlan1 eth1 eth2");
+			nvram_set("wan_ifnameX", "vlan2");
+			nvram_set("wl_ifname", "eth1");
+		}
+		break;
+	case MODEL_E1000v2:
 	case MODEL_WRT160Nv3:
 		// same as M10, M20, WRT310Nv2, E1000v1
 		mfr = "Linksys";
 		name = nvram_safe_get("boot_hw_model");
-		if (strcmp(name, "E100") == 0)
-			name = "E1000 v1";
+		ver = nvram_safe_get("boot_hw_ver");
+		if (nvram_match("boot_hw_model", "E100")){
+			name = "E1000";
+		}
+		if (nvram_match("boot_hw_model", "M10") || nvram_match("boot_hw_model", "M20")){
+			mfr = "Cisco";
+		}
 		features = SUP_SES | SUP_80211N | SUP_WHAM_LED;
 		if (!nvram_match("t_fix1", (char *)name)) {
 			nvram_set("lan_ifnames", "vlan1 eth1");
@@ -1246,7 +1316,7 @@ static int init_nvram(void)
 
 	if (name) {
 		nvram_set("t_fix1", name);
-		sprintf(s, "%s %s", mfr, name);
+		sprintf(s, "%s %s %s", mfr, name, ver);
 	}
 	else {
 		snprintf(s, sizeof(s), "%s %d/%s/%s/%s/%s", mfr, check_hw_type(),

release/src/router/rc/mtd.c
@@ -296,7 +296,12 @@ int mtd_write_main(int argc, char *argv[])
 	case 0x4E583233: // 32XN	E2000
 	case 0x4E303136: // 610N	WRT610N v2
 	case 0x4E583136: // 61XN	E3000
+	case 0x30303945: // E900	E900
 	case 0x30303145: // E100	E1000
+	case 0x30323145: // E120	E1200v1
+	case 0x32323145: // E122	E1200v2
+	case 0x30353145: // E150	E1500
+	case 0x30353531: // 1550	E1550
 	case 0x3031304D: // M010	M10
 	case 0x3032304D: // M020	M20
 	case 0x3036314E: // N160	WRT160N
@@ -304,6 +309,8 @@ int mtd_write_main(int argc, char *argv[])
 	case 0x4E303133: // 310N	WRT310N v1/v2
 //	case 0x32435745: // EWC2	WRT300N?
 	case 0x3035314E: // N150	WRT150N
+	case 0x58353245: // E25X	E2500
+	case 0x30303233: // 3200	E3200
 	case 0x30303234: // 4200	E4200
 		if (safe_fread(((char *)&cth) + 4, 1, sizeof(cth) - 4, f) != (sizeof(cth) - 4)) {
 			goto ERROR;

release/src/router/shared/id.c
@@ -31,6 +31,12 @@ WRTSL54GS           BCM4704_BCM5325F      0x042f       42        0x10      0x001
 WTR54GS v1, v2      BCM5350               0x456        56        0x10      0xb18       (source: BaoWeiQuan)
 WRT160Nv1           BCM4704_BCM5325F_EWC  0x0472       42        0x11      0x0010      boot_hw_model=WRT160N boot_hw_ver=1.0
 WRT160Nv3, M10      BCM4716               0x04cd       42        0x1700                boot_hw_model=WRT160N boot_hw_ver=3.0 (M10: boot_hw_model=M10 boot_hw_ver=1.0)
+E900                BCM53572              0x058e       42        0x1155                (64k/8MB flash)
+E1000v2/E1500       BCM5357               0xf53b       42        0x1100    0x0710      E1500(64k/8MB flash)
+E1000v2.1/E1200v1   BCM5357               0xf53a       42        0x1100    0x0710      E1200v1(64k/4MB flash)
+E1550               BCM5358U              0xc550       42        0x1100    0x0710      (60k/16MB flash)
+E2500               BCM5358U              0xf550       42        0x1101    0x0710      (60k/16MB)
+E3200               BCM47186              0xf52a       42        0x1100                (60k/16MB)
 WRT320N/E2000       BCM4717               0x04ef       42/66     0x1304/0x1305/0x1307  boardflags: 0x0040F10 / 0x00000602 (??)
 WRT610Nv2/E3000     BCM4718               0x04cf       42/??     ??                    boot_hw_model=WRT610N/E300
 E4200               BCM4718               0xf52c       42        0x1101                boot_hw_model=E4200
@@ -162,9 +168,18 @@ int check_hw_type(void)
 	case 0x052b:
 		if (nvram_match("boardrev", "0x1204")) return HW_BCM5357; //rt-n15u
 		if (nvram_match("boardrev", "02")) return HW_BCM47186; //WNR3500Lv2
+	case 0xf53a:
+	case 0xf53b:
 	case 0x0550: //RT-N10U and RT-N53
 	case 0x054d:
 		return HW_BCM5357;
+	case 0xf52a:
+		return HW_BCM47186;
+	case 0xf550:
+	case 0xc500:
+		return HW_BCM5358U;
+	case 0x058e:
+		return HW_BCM53572;
 #endif
 	}
 
@@ -303,6 +318,26 @@ int get_model(void)
 			return MODEL_WRT320N;
 		case HW_BCM4718:
 			return MODEL_WRT610Nv2;
+		case HW_BCM47186:
+			if (nvram_match("boot_hw_model", "E3200")) return MODEL_E3200;
+			break;
+		case HW_BCM5357:
+			if (nvram_match("boot_hw_model", "M10") && nvram_match("boot_hw_ver", "2.0"))
+				return MODEL_E1000v2;
+			if (nvram_match("boot_hw_model", "E1000")) return MODEL_E1000v2;
+			if (nvram_match("boot_hw_model", "E1200") && nvram_match("boot_hw_ver", "1.0"))
+				return MODEL_E1500;
+			if (nvram_match("boot_hw_model", "E1500")) return MODEL_E1500;
+			break;
+		case HW_BCM53572:
+			if (nvram_match("boot_hw_model", "E900")) return MODEL_E900;
+			if (nvram_match("boot_hw_model", "E1200") && nvram_match("boot_hw_ver", "2.0"))
+				return MODEL_E900;
+			break;
+		case HW_BCM5358U:
+			if (nvram_match("boot_hw_model", "E1550")) return MODEL_E1550;
+			if (nvram_match("boot_hw_model", "E2500")) return MODEL_E2500;
+			break;
 #endif
 		}
 		return MODEL_WRT54G;

commit 8abbf37055c1a02d16a59386eeefd31dad23a90c

fix advanced-vlan page
broken by commit ce6d38c0bbb8228e590476eec034faf9c929d1fe

release/src/router/www/advanced-vlan.asp
@@ -38,7 +38,7 @@
 <script type='text/javascript' src='wireless.jsx?_http_id=<% nv(http_id); %>'></script>
 <script type='text/javascript' src='interfaces.js'></script>
 <script type='text/javascript'>
-<% nvram ("vlan0ports,vlan1ports,vlan2ports,vlan3ports,vlan4ports,vlan5ports,vlan6ports,vlan7ports,vlan8ports,vlan9ports,vlan10ports,vlan11ports,vlan12ports,vlan13ports,vlan14ports,vlan15ports,vlan0hwname,vlan1hwname,vlan2hwname,vlan3hwname,vlan4hwname,vlan5hwname,vlan6hwname,vlan7hwname,vlan8hwname,vlan9hwname,vlan10hwname,vlan11hwname,vlan12hwname,vlan13hwname,vlan14hwname,vlan15hwname,wan_ifnameX,manual_boot_nv,boardtype,boardflags,trunk_vlan_so,lan_ifname,lan_ifnames,lan1_ifname,lan1_ifnames,lan2_ifname,lan2_ifnames,lan3_ifname,lan3_ifnames,boardrev");%> 
+<% nvram ("vlan0ports,vlan1ports,vlan2ports,vlan3ports,vlan4ports,vlan5ports,vlan6ports,vlan7ports,vlan8ports,vlan9ports,vlan10ports,vlan11ports,vlan12ports,vlan13ports,vlan14ports,vlan15ports,vlan0hwname,vlan1hwname,vlan2hwname,vlan3hwname,vlan4hwname,vlan5hwname,vlan6hwname,vlan7hwname,vlan8hwname,vlan9hwname,vlan10hwname,vlan11hwname,vlan12hwname,vlan13hwname,vlan14hwname,vlan15hwname,wan_ifnameX,manual_boot_nv,boardtype,boardflags,trunk_vlan_so,lan_ifname,lan_ifnames,lan1_ifname,lan1_ifnames,lan2_ifname,lan2_ifnames,lan3_ifname,lan3_ifnames,boardrev,boot_hw_ver,boot_hw_model");%>
 
 var port_vlan_supported = 0;
 var trunk_vlan_supported = 0;
@@ -98,7 +98,7 @@ switch(nvram['boardtype']) {
     break;
   case '0xf53a':  // E1000v2.1/E1200v1
   case '0xf53b':   // E1000v2/E1500
-   if (nvram['boot_hw_model'] == 'E1200' && nvram['boot_hw_ver'] == '1.0') || (nvram['boot_hw_model'] == 'E1500') {
+   if (((nvram['boot_hw_model'] == 'E1200') && (nvram['boot_hw_ver'] == '1.0')) || (nvram['boot_hw_model'] == 'E1500')) {
      COL_P0N = '0';
      COL_P1N = '1';
      COL_P2N = '2';