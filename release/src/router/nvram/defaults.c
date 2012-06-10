78c8fa34c90c01b1f445699282facd5f0c3dfed8
Marc Olivier Chouinard <mochouinard@moctel.com>
2012-06-10 05:14:32 -0300

    Add configuration options for the WAN ICMP Rate limiting
    Advanced->Firewall
    (cherry picked from commit c7d32a309c31f0f61abfd9506d8a49b9ac2bb687)

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -429,6 +429,9 @@ const defaults_t defaults[] = {
 //		{ "block_loopback",		"0"				},	// nat loopback
 	{ "nf_loopback",		"1"				},
 	{ "block_wan",			"1"				},	// block inbound icmp
+	{ "block_wan_limit",		"1"				},
+	{ "block_wan_limit_icmp",	"1"				},
+	{ "block_wan_limit_tr",		"5"				},
 	{ "multicast_pass",		"0"				},	// enable multicast proxy
 #ifdef TCONFIG_VLAN
 	{ "multicast_lan",		"0"				},	// on LAN (br0)
