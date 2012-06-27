18785bb5d8ee504c558d57590b5793cfdac8c2e9
Shibby <shibby@openlinksys.info>
2012-06-27 20:04:58 +0200

    fix menu
    fix menu when compiling only OpenVPN without PPTP or PPTP without PPTP Client.

--- release/src/router/www/tomato.js
+++ release/src/router/www/tomato.js
@@ -2439,14 +2439,17 @@ REMOVE-END */
 		['VPN Tunneling',			'vpn', 0, [
 /* OPENVPN-BEGIN */
 			['OpenVPN Server',		'server.asp'],
-			['OpenVPN Client',		'client.asp'],
+			['OpenVPN Client',		'client.asp']
 /* OPENVPN-END */
 /* PPTPD-BEGIN */
+/* OPENVPN-BEGIN */
+			,
+/* OPENVPN-END */
 			['PPTP Server',			'pptp-server.asp'],
-			['PPTP Online',			'pptp-online.asp'],
+			['PPTP Online',			'pptp-online.asp']
 /* PPTPD-END */
 /* USERPPTP-BEGIN */
-			['PPTP Client',		'pptp.asp']
+			,['PPTP Client',		'pptp.asp']
 /* USERPPTP-END */
 		] ],
 /* VPN-END */
