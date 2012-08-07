ec5c0f6e796c573fae190360dacc9560f88a6167
Jean-Yves Avenard <jean-yves@avenard.org>
2012-08-07 21:25:03 +1000

    Fix K24 build
    Revert some of the earlier MultiSSID changes for K24 build, it wouldn't have made a difference anyway

--- release/src/router/rc/network.c
+++ release/src/router/rc/network.c
@@ -366,7 +366,9 @@ void stop_lan_wl(void)
 	char *p, *ifname;
 	char *wl_ifnames;
 	char *lan_ifname;
+#ifdef CONFIG_BCMWL5
 	int unit, subunit;
+#endif
 
 	eval("ebtables", "-F");
 
@@ -414,7 +416,9 @@ void stop_lan_wl(void)
 void start_lan_wl(void)
 {
 	char *lan_ifname;
+#ifdef CONFIG_BCMWL5
 	struct ifreq ifr;
+#endif
 	char *wl_ifnames, *ifname, *p;
 	uint32 ip;
 	int unit, subunit, sta;
@@ -422,7 +426,11 @@ void start_lan_wl(void)
 	char tmp[32];
 	char br;
 
+#ifdef CONFIG_BCMWL5
 	foreach_wif(0, NULL, set_wlmac);
+#else
+	foreach_wif(1, NULL, set_wlmac);
+#endif
 
 	for(br=0 ; br<4 ; br++) {
 		char bridge[2] = "0";
@@ -468,9 +476,7 @@ void start_lan_wl(void)
 						if (get_ifname_unit(ifname, &unit, &subunit) < 0)
 							continue;
 						set_wlmac(0, unit, subunit, NULL);
-#endif
 					}
-#ifdef CONFIG_BCMWL5
 					else
 						wl_ioctl(ifname, WLC_GET_INSTANCE, &unit, sizeof(unit));
 #endif
@@ -577,6 +583,7 @@ void restart_wl(void)
 		xstart("radio", "join");
 }
 
+#ifdef CONFIG_BCMWL5
 static int disabled_wl(int idx, int unit, int subunit, void *param)
 {
 	char *ifname;
@@ -589,6 +596,7 @@ static int disabled_wl(int idx, int unit, int subunit, void *param)
 		return 1;
 	return 0;
 }
+#endif
 
 void start_wl(void)
 {
@@ -599,6 +607,7 @@ void start_wl(void)
 	char tmp[32];
 	char br;
 
+#ifdef CONFIG_BCMWL5
 		// HACK: When a virtual SSID is disabled, it requires two initialisation
 	if (foreach_wif(1, NULL, disabled_wl))
 	{
@@ -606,6 +615,7 @@ void start_wl(void)
 		start_wireless();
 		return;
 	}
+#endif
 
 	for(br=0 ; br<4 ; br++) {
 		char bridge[2] = "0";
@@ -716,7 +726,11 @@ void start_lan(void)
 	char *iftmp;
 	char nv[64];
 
+#ifdef CONFIG_BCMWL5
 	foreach_wif(0, NULL, set_wlmac);
+#else
+	foreach_wif(1, NULL, set_wlmac);
+#endif
 	check_afterburner();
 #ifdef TCONFIG_IPV6
 	enable_ipv6(ipv6_enabled());
