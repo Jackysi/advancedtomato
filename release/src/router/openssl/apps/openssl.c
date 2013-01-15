0ab3091d9dfa67842cf66a9ffaeb9fa8b2b31aa1
Shibby <shibby@openlinksys.info>
2013-01-15 11:23:29 +0100

    OpenSSL update to 1.0.1c: (clean source)

--- release/src/router/openssl/apps/openssl.c
+++ release/src/router/openssl/apps/openssl.c
@@ -471,16 +471,13 @@ static int do_cmd(LHASH_OF(FUNCTION) *prog, int argc, char *argv[])
 	fp=lh_FUNCTION_retrieve(prog,&f);
 	if (fp == NULL)
 		{
-#if 0
 		if (EVP_get_digestbyname(argv[0]))
 			{
 			f.type = FUNC_TYPE_MD;
 			f.func = dgst_main;
 			fp = &f;
 			}
-		else
-#endif // 0
-		if (EVP_get_cipherbyname(argv[0]))
+		else if (EVP_get_cipherbyname(argv[0]))
 			{
 			f.type = FUNC_TYPE_CIPHER;
 			f.func = enc_main;
