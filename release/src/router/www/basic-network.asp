Jackysi/advancedtomato

commit 80a9b7cd300a64c71f6d2bc701b88e9f1634c6e8

allow coexistance of N build and AC build.
src-rt = RT-N
src-rt-6.x = RT-AC

this commit include also (whole or part of them):
2506b224ad6685d855635abd732e506614fb4c35
2db33e2b612713b141129fff63625b9f160bfd98
826ffc413f87c0071db6d52ba9805863872b90f4
472cd2c0903f51eea9578348871f81313f19f1fa
7245e9ee236fdbda8a766ab9fca2178b56ac07a7
b1b8ed0f5f3d6c50c4ab382e05c88641b3df3ae6
024f036cacf8650e3167f6f79e1dba6ef9898fda
5adc665caedf2103d512c260ae8d461d1a904b4e
e5027a4e41f155616173ba6cc8de73b287a1d9bd
bb894ebf829539853d97f9d255c558dc671e519c

release/src/router/www/basic-network.asp
@@ -375,7 +375,6 @@ W('</style>');
 var xob = null;
 var refresher = [];
 var nphy = features('11n');
-var acphy = features('11ac');
 
 var ghz = [];
 var bands = [];
@@ -449,33 +448,6 @@ function refreshNetModes(uidx)
 	nm_loaded[uidx] = 1;
 }
 
-function refreshBandWidth(uidx)
-{
-	var e, i, buf, val;
-
-	if (uidx >= wl_ifaces.length) return;
-	var u = wl_unit(uidx);
-
-	var m = [['0','20 MHz']];
-	if(nphy || acphy){
-		m.push(['1','40 MHz']);
-	}
-	if(acphy && selectedBand(uidx) == '1') {
-		m.push(['3','80 MHz']);
-	}
-
-	e = E('_wl'+u+'_nbw_cap');
-	buf = '';
-	val = (!nm_loaded[uidx] || (e.value + '' == '')) ? eval('nvram.wl'+u+'_nbw_cap') : e.value;
-	for (i = 0; i < m.length; ++i)
-		buf += '<option value="' + m[i][0] + '"' + ((m[i][0] == val) ? ' selected' : '') + '>' + m[i][1] + '</option>';
-
-	e = E('__wl'+u+'_nbw_cap');
-	buf = '<select name="wl'+u+'_nbw_cap" onchange="verifyFields(this, 1)" id = "_wl'+u+'_nbw_cap">' + buf + '</select>';
-	elem.setInnerHTML(e, buf);
-	nm_loaded[uidx] = 1;
-}
-
 function refreshChannels(uidx)
 {
 	if (refresher[uidx] != null) return;
@@ -522,22 +494,10 @@ function refreshChannels(uidx)
 	e = E('_f_wl'+u+'_nctrlsb');
 	sb = (e.value + '' == '' ? eval('nvram.wl'+u+'_nctrlsb') : e.value);
 	e = E('_wl'+u+'_nbw_cap');
-	switch(e.value + '' == '' ? eval('nvram.wl'+u+'_nbw_cap') : e.value) {
-		case '0':
-			bw = '20';
-			break;
-		case '1':
-			bw = '40';
-			break;
-		case '3':
-			bw = '80';
-			break;
-		default:
-			alert("Wrong nbw_cap.");
-	}
+	bw = (e.value + '' == '' ? eval('nvram.wl'+u+'_nbw_cap') : e.value) == '0' ? '20' : '40';
 
 	refresher[uidx].onError = function(ex) { alert(ex); refresher[uidx] = null; reloadPage(); }
-	refresher[uidx].post('update.cgi', 'exec=wlchannels&arg0=' + u + '&arg1=' + (nphy || acphy ? '1' : '0') +
+	refresher[uidx].post('update.cgi', 'exec=wlchannels&arg0=' + u + '&arg1=' + (nphy ? '1' : '0') +
 		'&arg2=' + bw + '&arg3=' + selectedBand(uidx) + '&arg4=' + sb);
 }
 
@@ -734,7 +694,6 @@ function verifyFields(focused, quiet)
 			if (focused == E('_f_wl'+u+'_nband')) {
 				refreshNetModes(uidx);
 				refreshChannels(uidx);
-				refreshBandWidth(uidx);
 			}
 			else if (focused == E('_f_wl'+u+'_nctrlsb') || focused == E('_wl'+u+'_nbw_cap')) {
 				refreshChannels(uidx);
@@ -790,8 +749,8 @@ function verifyFields(focused, quiet)
 			_wl_ssid: 1,
 			_f_wl_bcast: 1,
 			_wl_channel: 1,
-			_wl_nbw_cap: nphy || acphy ? 1 : 0,
-			_f_wl_nctrlsb: nphy || acphy ? 1 : 0,
+			_wl_nbw_cap: nphy ? 1 : 0,
+			_f_wl_nctrlsb: nphy ? 1 : 0,
 			_f_wl_scan: 1,
 
 			_wl_security_mode: 1,
@@ -995,7 +954,7 @@ function verifyFields(focused, quiet)
 					wl_vis[uidx][a] = 2;
 				}
 				wl_vis[uidx]._f_wl_radio = 1;
-				wl_vis[uidx]._wl_nbw_cap = nphy || acphy ? 2 : 0;
+				wl_vis[uidx]._wl_nbw_cap = nphy ? 2 : 0;
 				wl_vis[uidx]._f_wl_nband = (bands[uidx].length > 1) ? 2 : 0;
 			}
 
@@ -1166,8 +1125,8 @@ REMOVE-END */
 			switch (E('_wl'+u+'_net_mode').value) {
 			case 'mixed':
 			case 'n-only':
-				if ((nphy || acphy) && (a.value == 'tkip') && (sm2.indexOf('wpa') != -1)) {
-					ferror.set(a, 'TKIP encryption is not supported with WPA / WPA2 in N and AC mode.', quiet || !ok);
+				if (nphy && (a.value == 'tkip') && (sm2.indexOf('wpa') != -1)) {
+					ferror.set(a, 'TKIP encryption is not supported with WPA / WPA2 in N mode.', quiet || !ok);
 					ok = 0;
 				}
 				else ferror.clear(a);
@@ -1425,7 +1384,7 @@ function save()
 			E('_wl'+u+'_nctrlsb').value = eval('nvram.wl'+u+'_nctrlsb');
 			if (E('_wl'+u+'_nmode').value != 0) {
 				E('_wl'+u+'_nctrlsb').value = E('_f_wl'+u+'_nctrlsb').value;
-				E('_wl'+u+'_nbw').value = (E('_wl'+u+'_nbw_cap').value == 0) ? 20 : ((E('_wl'+u+'_nbw_cap').value== 3) ? 80:40);
+				E('_wl'+u+'_nbw').value = (E('_wl'+u+'_nbw_cap').value == 0) ? 20 : 40;
 			}
 
 			E('_wl'+u+'_closed').value = E('_f_wl'+u+'_bcast').checked ? 0 : 1;
@@ -1542,7 +1501,6 @@ function init()
 		if (wl_sunit(uidx)<0) {
 			refreshNetModes(uidx);
 			refreshChannels(uidx);
-			refreshBandWidth(uidx);
 		}
 	}
 }
@@ -1731,8 +1689,8 @@ if (wl_sunit(uidx)<0) {
 		{ title: 'Broadcast', indent: 2, name: 'f_wl'+u+'_bcast', type: 'checkbox', value: (eval('nvram.wl'+u+'_closed') == '0') },
 		{ title: 'Channel', name: 'wl'+u+'_channel', type: 'select', options: ghz[uidx], prefix: '<span id="__wl'+u+'_channel">', suffix: '</span> <input type="button" id="_f_wl'+u+'_scan" value="Scan" onclick="scanButton('+u+')"> <img src="spin.gif" id="spin'+u+'">',
 			value: eval('nvram.wl'+u+'_channel') },
-		{ title: 'Channel Width', name: 'wl'+u+'_nbw_cap', type: 'select', options: [],
-			value: eval('nvram.wl'+u+'_nbw_cap'), prefix: '<span id="__wl'+u+'_nbw_cap">', suffix: '</span>' },
+		{ title: 'Channel Width', name: 'wl'+u+'_nbw_cap', type: 'select', options: [['0','20 MHz'],['1','40 MHz']],
+			value: eval('nvram.wl'+u+'_nbw_cap') },
 		{ title: 'Control Sideband', name: 'f_wl'+u+'_nctrlsb', type: 'select', options: [['lower','Lower'],['upper','Upper']],
 			value: eval('nvram.wl'+u+'_nctrlsb') == 'none' ? 'lower' : eval('nvram.wl'+u+'_nctrlsb') },
 		null,

release/src/router/www/tomato.js
@@ -2304,7 +2304,7 @@ function timeString(mins)
 
 function features(s)
 {
-	var features = ['ses','brau','aoss','wham','hpamp','!nve','11n','1000et','11ac'];
+	var features = ['ses','brau','aoss','wham','hpamp','!nve','11n','1000et'];
 	var i;
 
 	for (i = features.length - 1; i >= 0; --i) {

commit bda54f4f528e70b50c41e387e44857cb26476683

support 802.11AC in web settings - once again
broken by commit: 80a9b7cd300a64c71f6d2bc701b88e9f1634c6e8

release/src/router/www/basic-network.asp
@@ -375,6 +375,7 @@ W('</style>');
 var xob = null;
 var refresher = [];
 var nphy = features('11n');
+var acphy = features('11ac');
 
 var ghz = [];
 var bands = [];
@@ -448,6 +449,33 @@ function refreshNetModes(uidx)
 	nm_loaded[uidx] = 1;
 }
 
+function refreshBandWidth(uidx)
+{
+	var e, i, buf, val;
+
+	if (uidx >= wl_ifaces.length) return;
+	var u = wl_unit(uidx);
+
+	var m = [['0','20 MHz']];
+	if(nphy || acphy){
+		m.push(['1','40 MHz']);
+	}
+	if(acphy && selectedBand(uidx) == '1') {
+		m.push(['3','80 MHz']);
+	}
+
+	e = E('_wl'+u+'_nbw_cap');
+	buf = '';
+	val = (!nm_loaded[uidx] || (e.value + '' == '')) ? eval('nvram.wl'+u+'_nbw_cap') : e.value;
+	for (i = 0; i < m.length; ++i)
+		buf += '<option value="' + m[i][0] + '"' + ((m[i][0] == val) ? ' selected' : '') + '>' + m[i][1] + '</option>';
+
+	e = E('__wl'+u+'_nbw_cap');
+	buf = '<select name="wl'+u+'_nbw_cap" onchange="verifyFields(this, 1)" id = "_wl'+u+'_nbw_cap">' + buf + '</select>';
+	elem.setInnerHTML(e, buf);
+	nm_loaded[uidx] = 1;
+}
+
 function refreshChannels(uidx)
 {
 	if (refresher[uidx] != null) return;
@@ -494,10 +522,22 @@ function refreshChannels(uidx)
 	e = E('_f_wl'+u+'_nctrlsb');
 	sb = (e.value + '' == '' ? eval('nvram.wl'+u+'_nctrlsb') : e.value);
 	e = E('_wl'+u+'_nbw_cap');
-	bw = (e.value + '' == '' ? eval('nvram.wl'+u+'_nbw_cap') : e.value) == '0' ? '20' : '40';
+	switch(e.value + '' == '' ? eval('nvram.wl'+u+'_nbw_cap') : e.value) {
+		case '0':
+			bw = '20';
+			break;
+		case '1':
+			bw = '40';
+			break;
+		case '3':
+			bw = '80';
+			break;
+		default:
+			alert("Wrong nbw_cap.");
+	}
 
 	refresher[uidx].onError = function(ex) { alert(ex); refresher[uidx] = null; reloadPage(); }
-	refresher[uidx].post('update.cgi', 'exec=wlchannels&arg0=' + u + '&arg1=' + (nphy ? '1' : '0') +
+	refresher[uidx].post('update.cgi', 'exec=wlchannels&arg0=' + u + '&arg1=' + (nphy || acphy ? '1' : '0') +
 		'&arg2=' + bw + '&arg3=' + selectedBand(uidx) + '&arg4=' + sb);
 }
 
@@ -694,6 +734,7 @@ function verifyFields(focused, quiet)
 			if (focused == E('_f_wl'+u+'_nband')) {
 				refreshNetModes(uidx);
 				refreshChannels(uidx);
+				refreshBandWidth(uidx);
 			}
 			else if (focused == E('_f_wl'+u+'_nctrlsb') || focused == E('_wl'+u+'_nbw_cap')) {
 				refreshChannels(uidx);
@@ -749,8 +790,8 @@ function verifyFields(focused, quiet)
 			_wl_ssid: 1,
 			_f_wl_bcast: 1,
 			_wl_channel: 1,
-			_wl_nbw_cap: nphy ? 1 : 0,
-			_f_wl_nctrlsb: nphy ? 1 : 0,
+			_wl_nbw_cap: nphy || acphy ? 1 : 0,
+			_f_wl_nctrlsb: nphy || acphy ? 1 : 0,
 			_f_wl_scan: 1,
 
 			_wl_security_mode: 1,
@@ -954,7 +995,7 @@ function verifyFields(focused, quiet)
 					wl_vis[uidx][a] = 2;
 				}
 				wl_vis[uidx]._f_wl_radio = 1;
-				wl_vis[uidx]._wl_nbw_cap = nphy ? 2 : 0;
+				wl_vis[uidx]._wl_nbw_cap = nphy || acphy ? 2 : 0;
 				wl_vis[uidx]._f_wl_nband = (bands[uidx].length > 1) ? 2 : 0;
 			}
 
@@ -1125,8 +1166,8 @@ REMOVE-END */
 			switch (E('_wl'+u+'_net_mode').value) {
 			case 'mixed':
 			case 'n-only':
-				if (nphy && (a.value == 'tkip') && (sm2.indexOf('wpa') != -1)) {
-					ferror.set(a, 'TKIP encryption is not supported with WPA / WPA2 in N mode.', quiet || !ok);
+				if ((nphy || acphy) && (a.value == 'tkip') && (sm2.indexOf('wpa') != -1)) {
+					ferror.set(a, 'TKIP encryption is not supported with WPA / WPA2 in N and AC mode.', quiet || !ok);
 					ok = 0;
 				}
 				else ferror.clear(a);
@@ -1384,7 +1425,7 @@ function save()
 			E('_wl'+u+'_nctrlsb').value = eval('nvram.wl'+u+'_nctrlsb');
 			if (E('_wl'+u+'_nmode').value != 0) {
 				E('_wl'+u+'_nctrlsb').value = E('_f_wl'+u+'_nctrlsb').value;
-				E('_wl'+u+'_nbw').value = (E('_wl'+u+'_nbw_cap').value == 0) ? 20 : 40;
+				E('_wl'+u+'_nbw').value = (E('_wl'+u+'_nbw_cap').value == 0) ? 20 : ((E('_wl'+u+'_nbw_cap').value== 3) ? 80:40);
 			}
 
 			E('_wl'+u+'_closed').value = E('_f_wl'+u+'_bcast').checked ? 0 : 1;
@@ -1501,6 +1542,7 @@ function init()
 		if (wl_sunit(uidx)<0) {
 			refreshNetModes(uidx);
 			refreshChannels(uidx);
+			refreshBandWidth(uidx);
 		}
 	}
 }
@@ -1689,8 +1731,8 @@ if (wl_sunit(uidx)<0) {
 		{ title: 'Broadcast', indent: 2, name: 'f_wl'+u+'_bcast', type: 'checkbox', value: (eval('nvram.wl'+u+'_closed') == '0') },
 		{ title: 'Channel', name: 'wl'+u+'_channel', type: 'select', options: ghz[uidx], prefix: '<span id="__wl'+u+'_channel">', suffix: '</span> <input type="button" id="_f_wl'+u+'_scan" value="Scan" onclick="scanButton('+u+')"> <img src="spin.gif" id="spin'+u+'">',
 			value: eval('nvram.wl'+u+'_channel') },
-		{ title: 'Channel Width', name: 'wl'+u+'_nbw_cap', type: 'select', options: [['0','20 MHz'],['1','40 MHz']],
-			value: eval('nvram.wl'+u+'_nbw_cap') },
+		{ title: 'Channel Width', name: 'wl'+u+'_nbw_cap', type: 'select', options: [],
+			value: eval('nvram.wl'+u+'_nbw_cap'), prefix: '<span id="__wl'+u+'_nbw_cap">', suffix: '</span>' },
 		{ title: 'Control Sideband', name: 'f_wl'+u+'_nctrlsb', type: 'select', options: [['lower','Lower'],['upper','Upper']],
 			value: eval('nvram.wl'+u+'_nctrlsb') == 'none' ? 'lower' : eval('nvram.wl'+u+'_nctrlsb') },
 		null,

release/src/router/www/tomato.js
@@ -2304,7 +2304,7 @@ function timeString(mins)
 
 function features(s)
 {
-	var features = ['ses','brau','aoss','wham','hpamp','!nve','11n','1000et'];
+	var features = ['ses','brau','aoss','wham','hpamp','!nve','11n','1000et','11ac'];
 	var i;
 
 	for (i = features.length - 1; i >= 0; --i) {