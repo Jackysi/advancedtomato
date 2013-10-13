Jackysi/advancedtomato

commit ba939633055661c39950109d001512f9adf44d65

Miniupnpd: lanmask has to be present as bits

release/src/router/rc/services.c
@@ -813,29 +813,40 @@ void start_upnp(void)
 					char *lanip = nvram_safe_get(lanN_ipaddr);
 					char *lanmask = nvram_safe_get(lanN_netmask);
 					char *lanlisten = nvram_safe_get(upnp_lanN);
+
+					// Count bits of lanmask - shibby
+					unsigned int i, bit_mask=0;
+					struct in_addr mask;
+
+					inet_aton (lanmask, &mask);
+					//
+
+					for (i=0; i<32; i++)
+						bit_mask += (mask.s_addr>>i)%2;
+
 					if((strcmp(lanlisten,"1")==0) && (strcmp(lanip,"")!=0) && (strcmp(lanip,"0.0.0.0")!=0)) {
 #else
 					char *lanip = nvram_safe_get("lan_ipaddr");
 					char *lanmask = nvram_safe_get("lan_netmask");
 #endif
 						fprintf(f,
-							"listening_ip=%s/%s\n",
-							lanip, lanmask);
+							"listening_ip=%s/%d\n",
+							lanip, bit_mask);
 						int ports[4];
 						if ((ports[0] = nvram_get_int("upnp_min_port_int")) > 0 &&
 							(ports[1] = nvram_get_int("upnp_max_port_int")) > 0 &&
 							(ports[2] = nvram_get_int("upnp_min_port_ext")) > 0 &&
 							(ports[3] = nvram_get_int("upnp_max_port_ext")) > 0) {
 							fprintf(f,
-								"allow %d-%d %s/%s %d-%d\n",
+								"allow %d-%d %s/%d %d-%d\n",
 								ports[0], ports[1],
-								lanip, lanmask,
+								lanip, bit_mask,
 								ports[2], ports[3]
 							);
 						}
 						else {
 							// by default allow only redirection of ports above 1024
-							fprintf(f, "allow 1024-65535 %s/%s 1024-65535\n", lanip, lanmask);
+							fprintf(f, "allow 1024-65535 %s/%d 1024-65535\n", lanip, bit_mask);
 						}
 #ifdef TCONFIG_VLAN
 					}
@@ -1571,10 +1582,10 @@ static void start_ftpd(void)
 		/*
 		username<password<rights[<root_dir]
 		rights:
-			Read/Write
-			Read Only
-			View Only
-			Private
+			Read/Write - Odczyt/Zapis
+			Read Only - Odczyt
+			View Only - Widok
+			Private - Prywatne
 		*/
 		p = buf;
 		while ((q = strsep(&p, ">")) != NULL) {
@@ -1586,7 +1597,7 @@ static void start_ftpd(void)
 				root_dir = nvram_safe_get("ftp_pubroot");
 
 			/* directory */
-			if (strncmp(rights, "Private", 7) == 0)
+			if (strncmp(rights, "Prywatne", 7) == 0)
 			{
 				sprintf(tmp, "%s/%s", nvram_storage_path("ftp_pvtroot"), user);
 				mkdir_if_none(tmp);
@@ -1604,9 +1615,9 @@ static void start_ftpd(void)
 				tmp[0] = 0;
 				if (nvram_invmatch("ftp_dirlist", "1"))
 					strcat(tmp, "dirlist_enable=yes\n");
-				if (strstr(rights, "Read") || !strcmp(rights, "Private"))
+				if (strstr(rights, "Odczyt") || !strcmp(rights, "Prywatne"))
 					strcat(tmp, "download_enable=yes\n");
-				if (strstr(rights, "Write") || !strncmp(rights, "Private", 7))
+				if (strstr(rights, "Zapis") || !strncmp(rights, "Prywatne", 7))
 					strcat(tmp, "write_enable=yes\n");
 					
 				fputs(tmp, f);

commit c69cca710d672e180b3ad74a8f9230366ecd8ee1

fix ftp permissions
fixes errors introduced by shibby in commit ba939633055661c39950109d001512f9adf44d65

"Miniupnpd: lanmask has to be present as bits"

release/src/router/rc/services.c
@@ -1738,10 +1738,10 @@ static void start_ftpd(void)
 		/*
 		username<password<rights[<root_dir]
 		rights:
-			Read/Write - Odczyt/Zapis
-			Read Only - Odczyt
-			View Only - Widok
-			Private - Prywatne
+			Read/Write
+			Read Only
+			View Only
+			Private
 		*/
 		p = buf;
 		while ((q = strsep(&p, ">")) != NULL) {
@@ -1753,7 +1753,7 @@ static void start_ftpd(void)
 				root_dir = nvram_safe_get("ftp_pubroot");
 
 			/* directory */
-			if (strncmp(rights, "Prywatne", 7) == 0)
+			if (strncmp(rights, "Private", 7) == 0)
 			{
 				sprintf(tmp, "%s/%s", nvram_storage_path("ftp_pvtroot"), user);
 				mkdir_if_none(tmp);
@@ -1771,9 +1771,9 @@ static void start_ftpd(void)
 				tmp[0] = 0;
 				if (nvram_invmatch("ftp_dirlist", "1"))
 					strcat(tmp, "dirlist_enable=yes\n");
-				if (strstr(rights, "Odczyt") || !strcmp(rights, "Prywatne"))
+				if (strstr(rights, "Read") || !strcmp(rights, "Private"))
 					strcat(tmp, "download_enable=yes\n");
-				if (strstr(rights, "Zapis") || !strncmp(rights, "Prywatne", 7))
+				if (strstr(rights, "Write") || !strncmp(rights, "Private", 7))
 					strcat(tmp, "write_enable=yes\n");
 					
 				fputs(tmp, f);