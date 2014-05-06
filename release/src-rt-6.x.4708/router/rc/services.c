0d111e04d0717f8dd5c24bb1f6cf7f3a1c9f006b
Shibby <shibby@openlinksys.info>
2014-05-06 10:58:49 +0200

    R7000 cd ...
    Add missing Makefile.
    Revert some changes from last commit.

--- release/src-rt-6.x.4708/router/rc/services.c
+++ release/src-rt-6.x.4708/router/rc/services.c
@@ -2022,6 +2022,7 @@ static void start_media_server(void)
 		else {
 			if ((f = fopen(argv[2], "w")) != NULL) {
 				port = nvram_get_int("ms_port");
+				https = nvram_get_int("https_enable");
 				dbdir = nvram_safe_get("ms_dbdir");
 				if (!(*dbdir)) dbdir = NULL;
 				mkdir_if_none(dbdir ? : "/var/run/"MEDIA_SERVER_APP);
@@ -2033,7 +2034,7 @@ static void start_media_server(void)
 					"db_dir=%s/.db\n"
 					"enable_tivo=%s\n"
 					"strict_dlna=%s\n"
-					"presentation_url=http://%s:%s\n"
+					"presentation_url=http%s://%s:%s/nas-media.asp\n"
 					"inotify=yes\n"
 					"notify_interval=600\n"
 					"album_art_names=Cover.jpg/cover.jpg/AlbumArtSmall.jpg/albumartsmall.jpg/AlbumArt.jpg/albumart.jpg/Album.jpg/album.jpg/Folder.jpg/folder.jpg/Thumb.jpg/thumb.jpg\n"
@@ -2046,7 +2047,7 @@ static void start_media_server(void)
 					dbdir ? : "/var/run/"MEDIA_SERVER_APP,
 					nvram_get_int("ms_tivo") ? "yes" : "no",
 					nvram_get_int("ms_stdlna") ? "yes" : "no",
-					nvram_safe_get("lan_ipaddr"), nvram_safe_get("http_lanport")
+					https ? "s" : "", nvram_safe_get("lan_ipaddr"), nvram_safe_get(https ? "https_lanport" : "http_lanport")
 				);
 
 				// media directories
