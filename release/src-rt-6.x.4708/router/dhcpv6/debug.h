b4f19bb91b6761eb0b8a0ab671266477cdb89096
Tvlz <Tvlz@Tvlz>
2015-05-08 15:56:56 -0400

    Re-enable IPv6 Debug Logging by setting debug_ipv6=1 in nvram
    REVERT: WIDE-DHCPV6: reduce size by disabling debug logging at compile time
    Tomato Git Commit:54859dca1f85a97261b7d6e70b1247e0ca1ad3c5

--- release/src-rt-6.x.4708/router/dhcpv6/debug.h
+++ release/src-rt-6.x.4708/router/dhcpv6/debug.h
@@ -1,15 +1,10 @@
 #ifndef MY_DEBUG_H_
 #define MY_DEBUG_H_
 
-#ifndef MAX_DEBUG_LEVEL
-# define MAX_DEBUG_LEVEL	LOG_NOTICE
-#endif
-
 /* First include stdio.h, which may contain the prototype for the external dprintf.
  * We do not want that. We redefine dprintf to our local implementation. */
 #include <stdio.h>
-#define dprintf(level, args...) \
-	( { if ((level) <= MAX_DEBUG_LEVEL) my_dprintf((level), args); } )
+#define dprintf my_dprintf
 
 #ifndef __P
 # define __P(x) x
