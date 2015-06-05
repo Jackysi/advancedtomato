2787d3a9eba309af09ffb2f73dfc43efde8ec3f5
edrikk <edriss@hotmail.com>
2015-06-05 11:28:04 +0000

    The code has forever had a typo of 1500 for log-async while the max allowed value is really 100. No major impact as dnsmasq internally corrects, but it's good to fix the typo.

--- release/src-rt-6.x.4708/router/rc/services.c
+++ release/src-rt-6.x.4708/router/rc/services.c
@@ -462,8 +462,7 @@ void start_dnsmasq()
 	TRACE_PT("run dnsmasq\n");
 
 	// Default to some values we like, but allow the user to override them.
-	eval("dnsmasq", "-c", "1500", "--log-async");
-	
+	eval("dnsmasq", "-c", "100", "--log-async");	
 	eval("dnsmasq", "-c", "2048", "--cache-size");
 
 	if (!nvram_contains_word("debug_norestart", "dnsmasq")) {
