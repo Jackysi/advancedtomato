6d8b205026140fd4cfff80271c8a9945dc48a3e7
edrikk <edriss@hotmail.com>
2015-06-05 17:38:54 +0000

    hso: fix memory leak when device disconnects, tx_buffer should freed as well. Cherry Pick from Upstream: http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=295fc56f465ee8e013b2889e42094da9b2bd7125&context=6&ignorews=0&dt=0

--- release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/usb/hso.c
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/usb/hso.c
@@ -2320,6 +2320,7 @@ static void hso_serial_common_free(struct hso_serial *serial)
 
 	/* unlink and free TX URB */
 	usb_free_urb(serial->tx_urb);
+	kfree(serial->tx_buffer);
 	kfree(serial->tx_data);
 }
 
