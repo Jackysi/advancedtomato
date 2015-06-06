6420ac0c6ca50c033f81a46e1338147174eb5148
edrikk <edriss@hotmail.com>
2015-06-06 02:03:38 +0000

    Irrelevant Typo fix (in comment).  Aligns file with upstream.

--- release/src-rt-6.x.4708/router/miniupnpd/natpmp.c
+++ release/src-rt-6.x.4708/router/miniupnpd/natpmp.c
@@ -394,7 +394,7 @@ void ProcessIncomingNATPMPPacket(int s, unsigned char *msg_buff, int len,
 					if(r==0) {
 						if(strcmp(senderaddrstr, iaddr_old)==0
 						    && iport==iport_old) {
-							/* redirection allready existing */
+							/* redirection already existing */
 							syslog(LOG_INFO, "port %hu %s already redirected to %s:%hu, replacing",
 							       eport, (proto==IPPROTO_TCP)?"tcp":"udp", iaddr_old, iport_old);
 							/* remove and then add again */
