c423e5b8f5ec993d613d3f312e5b8af57e5cb11a
Shibby <shibby@openlinksys.info>
2015-07-08 12:17:47 +0200

    OpenVPN: update to 2.3.7

--- release/src-rt-6.x.4708/router/openvpn/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-aclocal -I m4
-libtoolize --copy --force
-autoheader
-automake -a -c --foreign
-autoconf
-[ ! -f .gitignore ]; touch .gitignore
-[ ! -f .gitattributes ]; touch .gitattributes
