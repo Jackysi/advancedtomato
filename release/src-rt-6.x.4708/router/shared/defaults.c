c9c424aa6907e4293d900ff5e3f0a809fd0d0a0f
Tvlz <Tvlz@Tvlz>
2015-07-19 13:56:27 -0400

    Remove commit:b78f59647c "ipv6: Other ISP Configuration button"     Use Native IPv6 setting instead

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -163,7 +163,6 @@ struct nvram_tuple router_defaults[] = {
 	{ "ipv6_6rd_borderrelay",	"68.113.165.1"			, 0 },	// 6RD border relay address
 	{ "ipv6_6rd_ipv4masklen",	"0"				, 0 },	// 6RD IPv4 mask length (0-30) checkme
 	{ "ipv6_vlan",			"0"				, 0 },	// Enable IPv6 on 1=LAN1 2=LAN2 4=LAN3
-	{ "ipv6_isp_opt",		"0"				, 0 },	// wan.c add eval option for dhcpd
 #endif
 
 	// Wireless parameters
