27aceeeea27ca0adf5a48ee16c9eb8cfe0adafab
Tvlz <Tvlz@Tvlz>
2015-08-01 12:01:34 +0200

    IPv6 Fixes
    IPv6: Send ia-na only when using DHCPv6-PD
    IPv6: Flush Global Routes at shutdown
    IPv6: Add default route to DHCPv6-PD for misconfigured ISPs, if needed

--- release/src/router/rc/wan.c
+++ release/src/router/rc/wan.c
@@ -865,7 +865,7 @@ void start_wan6_done(const char *wan_ifname)
 		break;
 	case IPV6_NATIVE_DHCP:
 		if (nvram_get_int("ipv6_isp_opt") == 1) {
-			eval("ip", "route", "add", "::/0", "dev", (char *)wan_ifname);
+			eval("ip", "route", "add", "::/0", "dev", (char *)wan_ifname, "metric", "2048"); //not needed? - workaround for misconfigured ISPs?
 		}
 		stop_dhcp6c();
 		start_dhcp6c();
