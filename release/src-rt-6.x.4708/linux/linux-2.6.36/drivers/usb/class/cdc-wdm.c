11714293c2461c003aee7fb87218b4cc4f6e397d
Shibby <shibby@openlinksys.info>
2015-10-04 11:45:17 +0200

    Revert: "cdc-wdm module update"
    945198ecd8109c92f78c7f89bf4612b6b79e7d84

--- release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/usb/class/cdc-wdm.c
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/usb/class/cdc-wdm.c
@@ -534,14 +534,11 @@ static int wdm_flush(struct file *file, fl_owner_t id)
 	struct wdm_device *desc = file->private_data;
 
 	wait_event(desc->wait, !test_bit(WDM_IN_USE, &desc->flags));
-
-	/* cannot dereference desc->intf if WDM_DISCONNECTING */
-	if (desc->werr < 0 && !test_bit(WDM_DISCONNECTING, &desc->flags)) {
+	if (desc->werr < 0)
 		dev_err(&desc->intf->dev, "Error in flush path: %d\n",
 			desc->werr);
-	}
 
-	return usb_translate_errors(desc->werr);
+	return desc->werr;
 }
 
 static unsigned int wdm_poll(struct file *file, struct poll_table_struct *wait)
