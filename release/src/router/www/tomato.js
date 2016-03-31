ab7e5f3b0b263db7c8a6c13e88e1a4d9aa4462d2
Toastman <toastman@galactic_core>
2016-04-01 03:04:50 +0700

    Remove IPTraffic Graph

--- release/src/router/www/tomato.js
+++ release/src/router/www/tomato.js
@@ -2427,7 +2427,6 @@ function navi()
 		['IP Traffic',			'ipt', 0, [
 			['Real-Time',			'realtime.asp'],
 			['Last 24 Hours',		'24.asp'],
-			['View Graphs',			'graphs.asp'],
 			['Transfer Rates',		'details.asp'],
 			['Daily',			'daily.asp'],
 			['Monthly',			'monthly.asp']
