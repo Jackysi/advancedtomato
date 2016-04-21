f55e56caa8ff95b675c0481af2cca3fdad8d458d
Toastman <toastman@galactic_core>
2016-04-22 03:01:33 +0700

    Reinstate CPU % usage to Status Overview page
    (removed by commit 6c6651bab5234ecc0b3c22d644be5a50ddc56135)

--- release/src/router/httpd/misc.c
+++ release/src/router/httpd/misc.c
@@ -446,11 +446,41 @@ void asp_etherstates(int argc, char **argv)
 	}
 }
 
+void asp_jiffies(int argc, char **argv)
+{
+	char sa[64];
+	FILE *a;
+	char *e = NULL;
+	char *f= NULL;
+
+	const char procstat[] = "/proc/stat";
+	if ((a = fopen(procstat, "r")) != NULL) {
+		fgets(sa, sizeof(sa), a);
+
+		e = sa;
+
+		if ((e = strchr(sa, ' ')) != NULL) e = e + 2;
+
+		if ((f = strchr(sa, 10)) != NULL) *f = 0;
+
+		web_printf("\njiffies = [ '");
+		web_printf("%s", e);
+		web_puts("' ];\n");
+		fclose(a);
+	}
+}
+
 void asp_sysinfo(int argc, char **argv)
 {
 	struct sysinfo si;
 	char s[64];
 	meminfo_t mem;
+
+	char sa[64];
+	FILE *a;
+	char *e = NULL;
+	char *f= NULL;
+	const char procstat[] = "/proc/stat";
 
 	char system_type[64];
 	char cpu_model[64];
@@ -497,6 +527,19 @@ void asp_sysinfo(int argc, char **argv)
 			cpu_model,
 			bogomips,
 			cpuclk);
+
+	if ((a = fopen(procstat, "r")) != NULL) {
+		fgets(sa, sizeof(sa), a);
+		e = sa;
+		if ((e = strchr(sa, ' ')) != NULL) e = e + 2;
+		if ((f = strchr(sa, 10)) != NULL) *f = 0;
+		web_printf(",\n\tjiffies: '");
+		web_printf("%s", e);
+		web_puts("'\n");
+		fclose(a);
+	} else {
+		web_puts("\n");
+	}
 
 	web_puts("};\n");
 }
