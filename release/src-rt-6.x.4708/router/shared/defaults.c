6573cac0e42d87748d450c3b3412ac19ef5eb254
Shibby <shibby@openlinksys.info>
2016-06-21 08:54:38 +0200

    VPN: allow to disable "nobind" option for VPN Client configuration
    ONLY FOR ADVANCED USERS
    
    Allow to remove "nobind" from configuration by changing nvram`s variable:
    vpn_client1_nobind
    vpn_client2_nobind
    
    value 1 : add "nobind" to config (default)
    value 0 : remove "nobind" from config

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -985,6 +985,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "vpn_client1_key",      ""              , 0 },
 	{ "vpn_client1_br",       "br0"           , 0 },
 	{ "vpn_client1_nopull",   "0"             , 0 },
+	{ "vpn_client1_nobind",   "1"             , 0 },
 	{ "vpn_client1_route",    "0"             , 0 },
 	{ "vpn_client1_routing_val", ""           , 0 },
 	{ "vpn_client2_poll",     "0"             , 0 },
@@ -1015,6 +1016,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "vpn_client2_key",      ""              , 0 },
 	{ "vpn_client2_br",       "br0"           , 0 },
 	{ "vpn_client2_nopull",   "0"             , 0 },
+	{ "vpn_client2_nobind",   "1"             , 0 },
 	{ "vpn_client2_route",    "0"             , 0 },
 	{ "vpn_client2_routing_val", ""           , 0 },
 #endif	// vpn
