d29eb419d30452af5b236c0969128b8cfecb6630
tsynik <tsynik@gmail.com>
2016-07-28 14:02:59 +0200

    switch entware (obsolete) to entware-ng

--- release/src-rt-6.x.4708/router/others/entware-install.sh
+++ release/src-rt-6.x.4708/router/others/entware-install.sh
@@ -8,44 +8,42 @@ then
 else
     mkdir /opt
 fi
-for folder in bin etc include lib sbin share tmp usr var
+for folder in bin etc/init.d lib/opkg sbin share tmp usr var/log var/lock var/run
 do
   if [ -d "/opt/$folder" ]
   then
     echo "Warning: Folder /opt/$folder exists!"
     echo "Warning: If something goes wrong please clean /opt folder and try again."
   else
-    mkdir /opt/$folder
+    mkdir -p /opt/$folder
   fi
 done
-[ -d "/opt/lib/opkg" ] || mkdir -p /opt/lib/opkg
-[ -d "/opt/var/lock" ] || mkdir -p /opt/var/lock
-[ -d "/opt/var/log" ] || mkdir -p /opt/var/log
-[ -d "/opt/var/run" ] || mkdir -p /opt/var/run
 
 echo "Info: Opkg package manager deployment..."
-cd /opt/bin
-wget http://qnapware.zyxmon.org/binaries-armv7/installer/opkg
+CURARCH="armv7"
+DLOADER="ld-linux.so.3"
+URL=http://pkg.entware.net/binaries/$CURARCH/installer
+wget $URL/opkg -O /opt/bin/opkg
 chmod +x /opt/bin/opkg
-cd /opt/etc
-wget http://qnapware.zyxmon.org/binaries-armv7/installer/opkg.conf
+wget $URL/opkg.conf -O /opt/etc/opkg.conf
+wget $URL/ld-2.23.so -O /opt/lib/ld-2.23.so
+wget $URL/libc-2.23.so -O/opt/lib/libc-2.23.so
+wget $URL/libgcc_s.so.1.2.23 -O /opt/lib/libgcc_s.so.1
 cd /opt/lib
-wget http://qnapware.zyxmon.org/binaries-armv7/installer/ld-2.20.so
-chmod +x ld-2.20.so
-ln -s ld-2.20.so ld-linux.so.3
-wget http://qnapware.zyxmon.org/binaries-armv7/installer/libc-2.20.so
-ln -s libc-2.20.so libc.so.6
+chmod +x ld-2.23.so
+ln -s ld-2.23.so $DLOADER
+ln -s libc-2.23.so libc.so.6
 
 echo "Info: Basic packages installation..."
 /opt/bin/opkg update
-/opt/bin/opkg install glibc-opt
+/opt/bin/opkg install entware-opt
 if [ ! -f /opt/usr/lib/locale/locale-archive ]
 then
-        wget http://qnapware.zyxmon.org/binaries-armv7/installer/locale-archive -O /opt/usr/lib/locale/locale-archive
+        wget http://pkg.entware.net/binaries/other/locale-archive.2.23 -O /opt/usr/lib/locale/locale-archive
 fi
 
 echo "Info: Congratulations!"
-echo "Info: If there are no errors above then Entware.arm successfully initialized."
+echo "Info: If there are no errors above then Entware-ng was successfully initialized."
 echo "Info: Add /opt/bin & /opt/sbin to your PATH variable"
-echo "Info: Add '/opt/etc/init.d/rc.unslung start' to startup script for Entware.arm services to start"
-echo "Info: Found a Bug? Please report at https://github.com/zyxmon/entware-arm/issues"
+echo "Info: Add '/opt/etc/init.d/rc.unslung start' to startup script for Entware-ng services to start"
+echo "Info: Found a Bug? Please report at https://github.com/Entware-ng/Entware-ng/issues"
