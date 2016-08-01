377f6717bcb6b71518b04f0a403497fedc373ce2
Tvlz <Tvlztomato@gmail.com>
2016-08-01 17:22:23 +0700

    Allow Incoming IPv6 IPSec by default
    Thx Tvlz

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -165,6 +165,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "ipv6_6rd_ipv4masklen",	"0"				, 0 },	// 6RD IPv4 mask length (0-30) checkme
 	{ "ipv6_vlan",			"0"				, 0 },	// Enable IPv6 on 1=LAN1 2=LAN2 4=LAN3
 	{ "ipv6_pdonly",		"0"				, 0 },	// Request DHCPv6 Prefix Delegation Only
+	{ "ipv6_ipsec",			"1"				, 0 },	// Enable Incoming IPv6 IPSec
 #endif
 
 #ifdef RTCONFIG_FANCTRL
