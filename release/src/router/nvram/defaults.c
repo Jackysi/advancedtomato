0759aa92d6171e7324911977354234aa6aee4b5b
Jeremy Chadwick <jdc@koitsu.org>
2017-05-03 14:09:01 +0200

    Add support for user-defined interfaces for Samba
        This adds a new NVRAM variable, smbd_ifnames, a space-delimited list of
        interface names to bind to.  It is used by the "interfaces" smb.conf
        directive.  The variable defaults to br0 (lan_ifname) in cases where
        the user has upgraded the FW without resetting NVRAM to defaults (i.e.
        smbd_ifnames is unset), or when leaving the GUI form field empty.
    
        Reference: https://www.samba.org/samba/docs/man/manpages-3/smb.conf.5.html
        Reference: https://www.samba.org/~tpot/articles/multiple-interfaces.html
        Reference: http://www.linksysinfo.org/index.php?threads/attempt-at-patching-smb-custom-vs-default-config-conflict-for-interfaces-option.73024/

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -925,6 +925,7 @@ const defaults_t defaults[] = {
 	},
 	{ "smbd_user",			"nas"				},
 	{ "smbd_passwd",		""				},
+	{ "smbd_ifnames",		"br0"				},
 #endif
 
 #ifdef TCONFIG_MEDIA_SERVER
--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -1932,6 +1932,7 @@ static void start_samba(void)
 	char nlsmod[15];
 	int mode;
 	char *nv;
+	char *si;
 
 	if (getpid() != 1) {
 		start_service("smbd");
@@ -1944,6 +1945,8 @@ static void start_samba(void)
 
 	if ((fp = fopen("/etc/smb.conf", "w")) == NULL)
 		return;
+
+	si = nvram_safe_get("smbd_ifnames");
 
 	fprintf(fp, "[global]\n"
 		" interfaces = %s\n"
@@ -1963,7 +1966,7 @@ static void start_samba(void)
 		" encrypt passwords = yes\n"
 		" preserve case = yes\n"
 		" short preserve case = yes\n",
-		nvram_safe_get("lan_ifname"),
+		strlen(si) ? si : nvram_safe_get("lan_ifname"),
 		nvram_get("smbd_wgroup") ? : "WORKGROUP",
 		nvram_safe_get("lan_hostname"),
 		nvram_get("router_name") ? : "Tomato",
